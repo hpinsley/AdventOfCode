4f46c4d863d3566706355db6f9c10cb290b017ea
Howard Pinsley <hpinsley@gmail.com>
2019-06-29 13:47:13 -0400

    Second problem

--- problem-2019-01/Program.fs
+++ problem-2019-01/Program.fs
@@ -1,14 +1,17 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open System.IO
 open FSharp.Data
 
 [<EntryPoint>]
 let main argv =
     let url = "https://adventofcode.com/2018/day/1/input"
-    printfn "Reading from %s\n" url
+    let textFile = "problem-2019-01/input.txt"
+    let total = File.ReadAllLines textFile
+                |> Array.map int
+                |> Array.toList
+                |> List.sum
 
-    let result = Http.RequestString url
-
+    printf "The total is %d\n" total
     0 // return an integer exit code
-
---
+++ problem-2019-02/Program.fs
@@ -0,0 +1,21 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open System.IO
+open FSharp.Data
+
+
+
+let getSum (file: string): int =
+    let total = File.ReadAllLines file
+                |> Array.map int
+                |> Array.toList
+                |> List.sum
+    total
+
+[<EntryPoint>]
+let main argv =
+    let textFile = "problem-2019-02/input.txt"
+    let total = getSum textFile
+    printf "The total for #3 is %d\n" total
+    0 // return an integer exit code
