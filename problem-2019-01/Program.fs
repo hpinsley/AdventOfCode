f51d4e6e303c20e202e589837904c92733688f19
Howard Pinsley <hpinsley@gmail.com>
2019-06-29 13:11:48 -0400

    Add package

--- problem-2019-01/Program.fs
+++ problem-2019-01/Program.fs
@@ -1,8 +1,14 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open FSharp.Data
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    let url = "https://adventofcode.com/2018/day/1/input"
+    printfn "Reading from %s\n" url
+
+    let result = Http.RequestString url
+
     0 // return an integer exit code
+
