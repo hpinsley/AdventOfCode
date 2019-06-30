572d5a3d366a8df92d63cd2a219f1a7289e5f76f
Howard Pinsley <hpinsley@gmail.com>
2019-06-30 13:51:21 -0400

    Setup for #5

---
+++ Problem-05/Program.fs
@@ -0,0 +1,19 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System.IO
+open System.Text.RegularExpressions
+
+let getData (file: string): array<string> =
+    printf "Reading from file %s\n" file
+    File.ReadAllLines file
+
+
+[<EntryPoint>]
+let main argv =
+    let textFile = "/Users/howard.pinsley/dev/adventofcode/problem-05/input.txt"
+    let values = getData textFile
+                  |> Array.toList
+
+    printfn "(5) Read %d lines" (List.length values)
+
+    0 // return an integer exit code
