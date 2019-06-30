4b1a09683f678f46a35aebe3ba50e82d31f146fb
Howard Pinsley <hpinsley@gmail.com>
2019-06-30 07:10:58 -0400

    Start problem 3

---
+++ Problem-03/Program.fs
@@ -0,0 +1,17 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System.IO
+
+
+let getData (file: string): array<string> =
+    printf "Reading from file %s\n" file
+    File.ReadAllLines file
+
+
+[<EntryPoint>]
+let main argv =
+    let textFile = "/Users/howard.pinsley/dev/adventofcode/problem-03/input.txt"
+    let values = getData textFile |> Array.toList
+
+    printfn "The file contains %d lines" (List.length values)
+    0 // return an integer exit code
