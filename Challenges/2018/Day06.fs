9539059a5dc65bf1027bf8df3ca87b4b0eedab75
Howard Pinsley <hpinsley@gmail.com>
2019-07-04 13:00:16 -0400

    Solved sample

--- Challenges/2018/Day06.fs
+++ Challenges/2018/Day06.fs
@@ -10,14 +10,14 @@ type CellState =
     | Unoccupied
     | Occupied of OccupiedCell
 
-type ClosestCoordinate =
-    | None
-    | Neighbor of OccupiedCell
+type Area =
+    | Finite of int
+    | Infinite
 
 let distance (x1, y1) (x2, y2) =
     (abs (x2 - x1)) + (abs (y2 - y1))
 
-let getCoords (inputLine: string) =
+let getPoints (inputLine: string) =
     let v = inputLine.Split(",")
                 |> Array.map parseInt
     (v.[0], v.[1])
@@ -36,29 +36,114 @@ let getBounds (points: seq<int * int>) =
 let setCoordinate (arr: CellState [,]) (x,y) =
     arr.[x, y] <- Occupied <| OccupiedCell (x, y)
 
-// Given the entire coordinate space, find the closest occ
-let getClosestCoord (arr:CellState [,]) (x: int) (y: int) coord =
-    Unoccupied
+let findAbsoluteMinimum (items: ((int * int) * int) list) =
+    let minValue = items |> List.map snd |> List.min
+    let mins = items
+                |> List.filter (fun ((x, y), len) -> len = minValue)
+                |> List.map (fun ((x, y), len) -> (x, y))
+
+    match mins with
+        | [loneWinner] -> Some loneWinner
+        | _ -> None
+
+
+// Given the entire coordinate space, find the closest.  We are location x,y
+// and determining the distances to all occupied cells.  If there is one with
+// a nearest distance, it wins.  If there is a tie, there is none.
+// each element of grid to see if it an occupied coordinate.  Note that we don't
+// actually need to use the coord parameter in this function
+let getClosestCoord (occupied: (int * int) list) (x: int) (y: int) (coord: CellState) : OccupiedCell option =
+
+    occupied
+        |> List.map (fun (x1, y1) -> ((x1, y1), distance (x1, y1) (x, y)))
+        |> findAbsoluteMinimum
+
+// Given an (x,y) coordinate and the closest grid, determine the area
+let getArea (closestCoord: ((int * int ) option [,])) (x:int, y:int): Area =
+    let rows = Array2D.length1 closestCoord
+    let cols = Array2D.length2 closestCoord
+    let onEdge (x, y) =
+        x = 0 || y = 0 || x = (rows - 1) || y = (cols - 1)
+    let closestToCoord =
+        closestCoord
+            |> Seq.cast<(int * int) option>
+            |> Seq.choose (fun closest ->
+                                match closest with
+                                | Some (x1, y1) ->
+                                    if (x1 = x && y1 = y)
+                                        then Some (x1, y1)
+                                        else None
+
+                                | None -> None)
+            |> List.ofSeq
+
+    if List.exists onEdge closestToCoord
+    then
+        Infinite
+    else
+        Finite closestToCoord.Length
+
+let printOccupied (cellState: CellState [,]) =
+    let rows = Array2D.length1 cellState
+    let cols = Array2D.length2 cellState
+
+    for r in [0..(rows - 1)] do
+        printfn ""
+        for c in [0..(cols - 1)] do
+            printf "%s" <|
+                match cellState.[r, c] with
+                    | Occupied (x, y) -> (sprintf "[%d,%d]" x y)
+                    | Unoccupied -> "[.,.]"
+
+let printClosest (closestCoord: (int * int ) option [,]) =
+    let rows = Array2D.length1 closestCoord
+    let cols = Array2D.length2 closestCoord
+
+    for r in [0..(rows - 1)] do
+        printfn ""
+        for c in [0..(cols - 1)] do
+            printf "%s" <|
+                match closestCoord.[r, c] with
+                    | Some (x, y) -> (sprintf "[%d,%d]" x y)
+                    | _ -> "[.,.]"
 
 let solve =
     // let testdata = Common.getChallengeData 2018 6
     let testdata = Common.getSampleDataAsArray 2018 6
 
-    let points = testdata |> Array.map getCoords
+    let points = testdata |> Array.map getPoints |> List.ofArray
 
-    Array.map (printfn "%O") points |> ignore
+    // List.map (printfn "%O") points |> ignore
     let bounds = getBounds points
     printfn "Bounds: %O" bounds
 
-    let arr =
+    let state =
         Array2D.create (xCoord bounds) (yCoord bounds) Unoccupied
 
     points |>
-        Array.map (setCoordinate arr) |> ignore
+        List.map (setCoordinate state) |> ignore
     // arr.[0,0] <- 5
 
-    let closestCoord =
-        Array2D.mapi (getClosestCoord arr) arr
 
-    printfn "Array is\n%A" arr
+    // printfn "Array is\n%A" arr
+
+    let closestCoord =
+        Array2D.mapi (getClosestCoord points) state
+
+    // printOccupied state
+    // printfn "\n-------"
+    // printClosest closestCoord
+
+    let biggestArea =
+        points
+            |> List.map (fun (x,y) ->
+                            ((x,y), getArea closestCoord (x,y))
+                        )
+            |> List.choose (fun ((x,y), area) ->
+                                match area with
+                                    | Finite a -> Some ((x,y), a)
+                                    | Infinite -> None)
+            |> List.maxBy (fun ((x, y), a) -> a)
+
+    printfn "\n\n%A" biggestArea
     ()
