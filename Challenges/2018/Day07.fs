1f6070d1e072acc90a32e8201883ab4c0788504a
Howard Pinsley <hpinsley@gmail.com>
2019-07-05 08:45:40 -0400

    Builds a dependency tree

--- Challenges/2018/Day07.fs
+++ Challenges/2018/Day07.fs
@@ -4,6 +4,20 @@ open System
 open System.IO
 open Common
 
+
+let buildDependencies (successors: (char * char list) list) (newPair: (char * char)): (char * char list) list =
+    let rec addPair (dependencies: (char * char list) list) (newPair: (char * char)): (char * char list) list =
+        let (newpred, newsucc) = newPair
+        match dependencies with
+            | [] -> [newsucc, [newpred]]
+            | (s, plist) as firstDependency :: remainingDependencies ->
+                if (s = newsucc) then
+                    (s, newpred :: plist) :: remainingDependencies
+                else
+                    firstDependency :: addPair remainingDependencies newPair
+
+    addPair successors newPair
+
 let solve =
     //let testdata = Common.getChallengeDataAsArray 2018 7
     let testdata = Common.getSampleDataAsArray 2018 7
@@ -12,4 +26,9 @@ let solve =
                     |> List.ofArray
                     |> List.map (fun line -> (line.[5], line.[36]))
                     |> dump "pairs"
+
+    let dependencies = pairs
+                        |> List.fold buildDependencies []
+
+    dump "Dependencies" dependencies
     ()
