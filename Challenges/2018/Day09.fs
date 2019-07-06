57c07f39d4cae1c7d70303974529d89a25cb3a3f
Howard Pinsley <hpinsley@gmail.com>
2019-07-06 13:34:35 -0400

    Much faster.  Changes the game board to a mutable array and using Array.Copy.

--- Challenges/2018/Day09.fs
+++ Challenges/2018/Day09.fs
@@ -9,7 +9,8 @@ type GameState = {
     lastMarbleValue: int;
     playerScores: int list;
     currentMarbleIndex: int;
-    board: int list;
+    numberOfMarblesOnBoard: int;
+    board: int [];
     currentPlayer: int
 }
 
@@ -25,39 +26,49 @@ let buildInitialBoard numberOfPlayers lastMarbleValue =
         lastMarbleValue = lastMarbleValue;
         playerScores = List.init numberOfPlayers (fun _ -> 0)
         currentMarbleIndex = 0;
-        board = [0];
+        numberOfMarblesOnBoard = 1;
+        board = Array.zeroCreate lastMarbleValue
         currentPlayer = 0;
     }
 
 let playStandard (game:GameState) (marbleValue:int): GameState =
-    let clockPlusOne = clockwiseFrom game.currentMarbleIndex 1 game.board.Length
-    let clockPlusTwo = clockwiseFrom game.currentMarbleIndex 2 game.board.Length
-    let splitIndex = if (clockPlusTwo > clockPlusOne) then clockPlusTwo else clockPlusOne + 1
-    let (l1, l2) = List.splitAt splitIndex game.board
-    let newBoard = List.concat [l1; [marbleValue]; l2]
-    let newCurrentIndex = splitIndex % newBoard.Length
+    let clockPlusOne = clockwiseFrom game.currentMarbleIndex 1 game.numberOfMarblesOnBoard
+    let clockPlusTwo = clockwiseFrom game.currentMarbleIndex 2 game.numberOfMarblesOnBoard
+
+    let newIndex = if clockPlusTwo > clockPlusOne then clockPlusTwo else clockPlusOne + 1
+    // We want to place a marble at newIndex.  Shift the array right
+
+    let itemsToCopy = game.numberOfMarblesOnBoard - newIndex
+    let _ = Array.Copy (game.board, newIndex, game.board, newIndex + 1, itemsToCopy)
+
+    game.board.[newIndex] <- marbleValue
+
+    let numberOfMarblesOnBoard = game.numberOfMarblesOnBoard + 1
     let nextPlayer = (game.currentPlayer + 1) % game.numberOfPlayers
 
-    { game with board = newBoard; currentMarbleIndex = newCurrentIndex; currentPlayer = nextPlayer }
+    { game with currentMarbleIndex = newIndex; numberOfMarblesOnBoard = numberOfMarblesOnBoard; currentPlayer = nextPlayer }
 
 let play23 (game:GameState) (marbleValue:int): GameState =
-    let counterBy7 = counterClockwiseFrom game.currentMarbleIndex 7 game.board.Length
-    let moveScore = marbleValue + game.board.[counterBy7];
+    let removeIndex = counterClockwiseFrom game.currentMarbleIndex 7 game.numberOfMarblesOnBoard
+
+    let removeScore = game.board.[removeIndex]
+    let moveScore = marbleValue + removeScore
+
+    // printfn "Scored %d + %d = %d (from index removed at %d)" marbleValue removeScore moveScore removeIndex
+
     let newScores = List.mapi (fun i score -> if i = game.currentPlayer then score + moveScore else score) game.playerScores
-    let (l, r) = List.splitAt counterBy7 game.board
-    let newBoard = match r with
-                    | toDrop :: rest ->
-                        List.concat [l; rest]
-                    | [] ->
-                        match l with
-                            | toDrop :: rest ->
-                                rest
-                            | _ ->
-                                l
-    let newCurrentIndex = counterBy7 % newBoard.Length
+
+    // We want to remove the marble at newIndex.  Shift the array left
+
+    let itemsToCopy = game.numberOfMarblesOnBoard - removeIndex - 1
+
+    Array.Copy (game.board, removeIndex + 1, game.board, removeIndex, itemsToCopy)
+
+    let numberOfMarblesOnBoard = game.numberOfMarblesOnBoard - 1
+
     let nextPlayer = (game.currentPlayer + 1) % game.numberOfPlayers
 
-    { game with playerScores = newScores; board = newBoard; currentMarbleIndex = newCurrentIndex; currentPlayer = nextPlayer }
+    { game with playerScores = newScores; numberOfMarblesOnBoard = numberOfMarblesOnBoard; currentMarbleIndex = removeIndex; currentPlayer = nextPlayer }
 
 let playMarble (game:GameState) (marbleValue:int): GameState =
     //printfn "Playing marble %d with board\n%A" marbleValue game
@@ -74,13 +85,16 @@ let solvePartOne  =
     //let initialBoard = buildInitialBoard 17 1104
     //let initialBoard = buildInitialBoard 21 6111
     //let initialBoard = buildInitialBoard 30 5807
-    let initialBoard = buildInitialBoard 428 72061
+    //let initialBoard = buildInitialBoard 428 72061
+    let initialBoard = buildInitialBoard 428 7206100
     printfn "Part one"
 
     //dump "Initial board" initialBoard
 
     let finalBoard = [1..initialBoard.lastMarbleValue]
                         |> List.fold playMarble initialBoard
+
+    // dump "player scores" finalBoard.playerScores
 
     printfn "Max score is %d" <| List.max finalBoard.playerScores
     //dump "Final board" finalBoard
