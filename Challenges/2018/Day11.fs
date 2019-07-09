182f90bca1deeb88f4208afbdb86d1b3b1b4e293
Howard Pinsley <hpinsley@gmail.com>
2019-07-09 07:30:28 -0400

    Created required functions

--- Challenges/2018/Day11.fs
+++ Challenges/2018/Day11.fs
@@ -10,15 +10,24 @@ let extractHundreds n =
     else
         (n / 100) % 10
 
-let power x y serialNumber =
+let power serialNumber x y  =
     let rackId = x + 10
     (rackId * y + serialNumber) * rackId
         |> extractHundreds
         |> (+) -5
+
+let gridPower serialNumber x y  =
+    seq { for x1 in x..x+2 do for y1 in y..y+2 -> (x1, y1) }
+        |> Seq.sumBy (fun (x,y) -> power serialNumber x y)
+
+let computeCell serialNumber = gridPower serialNumber
+
 let solvePartOne =
-    power 122 79 57 |> printfn "%A"
-    power 217 196 39 |> printfn "%A"
-    power 101 153 71 |> printfn "%A"
+    printfn "%A" (gridPower 18 33 45)
+    printfn "%A" (gridPower 42 21 61)
+
+    let fn = computeCell 42
+    printfn "%A" (fn 21 61)
     ()
 
 let solve =
