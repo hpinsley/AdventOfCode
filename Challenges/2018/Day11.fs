2fbe7a75b81bb0f4cd84b2133312681eb32409a4
Howard Pinsley <hpinsley@gmail.com>
2019-07-11 08:15:52 -0400

    wip

--- Challenges/2018/Day11.fs
+++ Challenges/2018/Day11.fs
@@ -2,6 +2,7 @@
 
 open System
 open System.IO
+open System.Collections.Generic
 open Common
 open System.Text.RegularExpressions
 
@@ -10,15 +11,36 @@ let extractHundreds n =
     else
         (n / 100) % 10
 
-let power serialNumber x y  =
+let powerHard serialNumber x y  =
     let rackId = x + 10
     (rackId * y + serialNumber) * rackId
         |> extractHundreds
         |> (+) -5
 
+let memoizePowerFunction () =
+    let cache = Dictionary<(int*int*int),int>()
+    printfn "Created cache"
+    let f sn x y =
+        let t = (sn,x,y)
+        if (cache.ContainsKey(t)) then
+            cache.[t]
+        else
+            let v = powerHard sn x y
+            cache.[t] <- v
+            v
+    f
+
+let powerFun = memoizePowerFunction()
+
+let sizeGenerator maxSize =
+    seq { for s in 1 .. maxSize do
+            printfn "Starting size %d" s
+            yield s
+        }
+
 let gridPower serialNumber x y size =
     seq { for x1 in x..x+(size-1) do for y1 in y..y+(size-1) -> (x1, y1) }
-        |> Seq.sumBy (fun (x,y) -> power serialNumber x y)
+        |> Seq.sumBy (fun (x,y) -> powerFun serialNumber x y)
 
 //let computeCell serialNumber = gridPower serialNumber
 
@@ -43,6 +65,8 @@ let solvePartTwo () =
     let folder (dict:Map<(int * int * int), int>) v =
         let (x,y,s) = v
 
+        // let powerFun = memoizePowerFunction()
+
         let result =
             match Map.tryFind (x, y, s - 1) dict with
                 | None ->
@@ -51,14 +75,14 @@ let solvePartTwo () =
                     subtotal +
                         (
                             seq { for x1 in x..x+s-1 -> (x1, y + s - 1) }
-                                |> Seq.sumBy (fun (x0, y0) -> power gridSerialNumber x0 y0)
+                                |> Seq.sumBy (fun (x0, y0) -> powerFun gridSerialNumber x0 y0)
 
                         )
                         +
                         (
                             // Don't double count the bottom right corner
                             seq { for y1 in y..y+s-2 -> (x + s - 1, y1) }
-                                |> Seq.sumBy (fun (x0, y0) -> power gridSerialNumber x0 y0)
+                                |> Seq.sumBy (fun (x0, y0) -> powerFun gridSerialNumber x0 y0)
                         )
 
         let newMap = Map.add (x,y,s) result dict
@@ -68,7 +92,7 @@ let solvePartTwo () =
 
     let finalValues =
         seq {
-            for s in 1..20 do
+            for s in sizeGenerator 20 do
             for x in 1..(300-(s-1)) do
             for y in 1..(300-(s-1)) -> (x,y,s)
         } |> Seq.fold folder initialDict
