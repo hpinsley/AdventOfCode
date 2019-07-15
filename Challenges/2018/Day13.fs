4cf8057e282a732bbf71e8d0c17bce2cb1df574b
Howard Pinsley <hpinsley@gmail.com>
2019-07-15 07:29:53 -0400

    Setup the files for 2018 day 13

---
+++ Challenges/2018/Day13.fs
@@ -0,0 +1,24 @@
+﻿module Year2018Day13
+
+open System
+open System.IO
+open Common
+open System.Text.RegularExpressions
+
+let solvePartOne () =
+    printfn "Starting part 1"
+    ()
+
+let solvePartTwo () =
+    printfn "Starting part 2"
+    printfn "End of part 2"
+    ()
+
+let solve() =
+    //let testdata = Common.getChallengeDataAsArray 2018 13
+    let testdata = Common.getSampleDataAsArray 2018 13
+    dump "data" testdata
+
+    solvePartOne()
+    //solvePartTwo()
+    ()
