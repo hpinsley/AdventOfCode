d31e36dbfdb3f7a8e72cac8da089eada72397fb9
Howard Pinsley <hpinsley@gmail.com>
2019-07-23 20:34:13 -0400

    wip

--- Challenges/2018/Day15.fs
+++ Challenges/2018/Day15.fs
@@ -20,6 +20,14 @@ type Cell =
     | Occupied of Player
     | Wall
 
+type State =
+    {
+        grid: Cell[,];
+        rounds: int;
+        waitingToPlay: Player list;
+        activePlayer: Player option;
+    }
+
 let getPlayerLetter (playerType: PlayerType) : char =
     match playerType with
         | Elf -> 'E'
@@ -43,11 +51,28 @@ let mapCellToChar (c:Cell) : char =
 let printGrid grid =
     Common.printGrid grid mapCellToChar
 
-let buildInitialGame (lines:string[]) : Cell[,] =
+let getIndexList (game:Cell[,]) : (int * int) list =
+    let rows = Array2D.length1 game
+    let cols = Array2D.length2 game
+    [for r in 0..rows-1 do for c in 0..cols - 1 do yield (r,c)]
+
+let getPlayersInOrder (game:Cell[,]) =
+    getIndexList game
+        |> List.choose (fun (r,c) ->
+                            match game.[r,c] with
+                                | Occupied player -> Some player
+                                | _ -> None)
+        |> List.sortBy (fun player -> (player.row, player.col))
+
+let printState (state:State) : unit =
+    printGrid state.grid
+    printPlayers (getPlayersInOrder state.grid)
+
+let buildInitialState (lines:string[]) : State =
 
     let rows = lines.Length
     let cols = lines |> Array.map (fun line -> line.Length) |> Array.max
-    let game = Array2D.init rows cols (fun row col ->
+    let grid = Array2D.init rows cols (fun row col ->
                                         match lines.[row].[col] with
                                             | '.' -> Empty
                                             | '#' -> Wall
@@ -68,28 +93,19 @@ let buildInitialGame (lines:string[]) : Cell[,] =
                                             | _ -> failwith "Unexpected character"
                                         )
 
-    game
+    {
+        grid = grid;
+        rounds = 0;
+        waitingToPlay = [];
+        activePlayer = None
+    }
 
-let getIndexList (game:Cell[,]) : (int * int) list =
-    let rows = Array2D.length1 game
-    let cols = Array2D.length2 game
-    [for r in 0..rows-1 do for c in 0..cols - 1 do yield (r,c)]
-
-let getPlayers (game:Cell[,]) =
-    getIndexList game
-        |> List.choose (fun (r,c) ->
-                            match game.[r,c] with
-                                | Occupied player -> Some player
-                                | _ -> None)
 
 let solve() =
     printfn "Day 15"
     //let testdata = Common.getChallengeDataAsArray 2018 15
     let testdata = Common.getSampleDataAsArray 2018 15
     //dump "data" testdata
-    let game = buildInitialGame testdata
-
-    printGrid game
-
-    printPlayers (getPlayers game)
+    let state = buildInitialState testdata
+    printState state
     ()
