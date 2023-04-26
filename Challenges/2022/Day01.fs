8798eba84bb86c1319e81e281366eb999e0ebe30
Howard Pinsley <howard.pinsley@blackstone.com>
2023-04-26 07:26:23 -0400

    Day one complete - 2 stars

--- Challenges/2022/Day01.fs
+++ Challenges/2022/Day01.fs
@@ -1,36 +1,33 @@
 ﻿module Year2022Day01
 
+open System
 open System.IO
-
-let getRepeatingValues (file: string): array<int> =
-    printf "Reading from file %s\n" file
-
-    File.ReadAllLines file
-        |> Array.map int
-
-let rec computeDup (iteration: int) (priorSum: int) (seen: Set<int>) (values: array<int>): int =
-
-    let length = Array.length values
-    let num = Array.get values (iteration % length)
-    let nextSum = priorSum + num
-
-    let _ = if iteration % 1000 = 0
-            then
-                printfn "Iteration: %d is (%d, %d)" iteration num nextSum
-            else
-                ()
-
-
-    if (Set.contains nextSum seen)
-        then nextSum
-        else
-            computeDup (iteration + 1) nextSum (Set.add nextSum seen) values
+open Common
+open System.Text.RegularExpressions
 
 
 let solve =
-    let textFile = "InputFiles/input-2018-01.txt"
 
-    let values = getRepeatingValues textFile
-    let firstRepeat = computeDup 0 0 (Set.empty) values
+    // let values = Common.getSampleDataAsArray 2022 01
+    let values = Common.getChallengeDataAsArray 2022 01
+    let calories = values |> Array.map 
+                        (fun v -> match v with
+                                                    | "" -> -1
+                                                    | _ -> parseInt v
+                                                )
+                        |> List.ofArray
 
-    printf "The first repeat is %d" firstRepeat
+    let terminated = List.append calories [-1]
+
+    let result = 
+            terminated 
+                |> Seq.fold (fun (elfCals, cumcals) cals  -> 
+                                    if (cals = -1) then (List.append elfCals [cumcals], 0)
+                                    else (elfCals, cumcals + cals)
+                                ) ([], 0)
+
+    let m = List.max (fst result)
+    printfn "%A" m
+
+    let top3 = result |> fst |> List.sortDescending |> Seq.take 3 |> Seq.sum
+    printfn "%A" top3
