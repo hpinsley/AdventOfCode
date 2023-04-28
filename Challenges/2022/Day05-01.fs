c0a076fcfbbb57fec3db010cd2ec8164fb61ad7e
Howard Pinsley <howard.pinsley@blackstone.com>
2023-04-28 18:37:31 -0400

    Set up for day 5

---
+++ Challenges/2022/Day05-01.fs
@@ -0,0 +1,12 @@
+﻿module Year2022Day05_Part1
+
+open System
+open System.IO
+open Common
+open System.Text.RegularExpressions
+
+let solve =
+
+    let lines = Common.getSampleDataAsArray 2022 05
+    // let lines = Common.getChallengeDataAsArray 2022 05
+    printfn "%A" lines  
