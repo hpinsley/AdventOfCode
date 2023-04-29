52c2715fbe7d5595568391a10d7367adbb1d9bb3
Howard Pinsley <howard.pinsley@blackstone.com>
2023-04-29 16:53:04 -0400

    WIP: break up the sections.

--- Challenges/2022/Day05-01.fs
+++ Challenges/2022/Day05-01.fs
@@ -7,6 +7,27 @@ open System.Text.RegularExpressions
 
 let solve =
 
-    let lines = Common.getSampleDataAsArray 2022 05
-    // let lines = Common.getChallengeDataAsArray 2022 05
-    printfn "%A" lines  
+    // let lines = Common.getSampleDataAsArray 2022 05
+    let lines = Common.getChallengeDataAsArray 2022 05
+    // printfn "%A" lines
+
+    let (reversedBlocks, _, reversedMoves) = lines 
+                                                |> Seq.fold (fun (blocks, haveSeenBlank, moves) line -> 
+                                                                    if (haveSeenBlank) then (blocks, haveSeenBlank, line :: moves)
+                                                                    else if (line.Trim().Length = 0) then (blocks, true, moves)
+                                                                    else (line :: blocks, haveSeenBlank, moves)
+                                                            ) ([], false, [])
+    let blocks = reversedBlocks |> List.rev
+    let moves = reversedMoves |> List.rev
+
+    for b in blocks do
+        printfn "%s" b
+
+    printfn ("\nMoves:\n")
+
+    for m in moves do
+        printfn "%s" m
+
+
+
+
