5c9f70774d54d3a13020a55314354d6dd2d1ee92
Howard Pinsley <howard.pinsley@blackstone.com>
2023-05-01 08:16:09 -0400

    Day 6 complete - 2 stars

--- Challenges/2022/Day06-01.fs
+++ Challenges/2022/Day06-01.fs
@@ -11,8 +11,26 @@ type Instruction = {
     ToCrate: int
 }
 
+let isMarker (s:string) : bool =
+    let sorted = s |> Seq.sort |> Array.ofSeq |> System.String
+    let distinct = sorted |> Seq.distinct |> Seq.sort |> Array.ofSeq |> System.String
+    let result = distinct.Equals(sorted)
+    //printfn "%A %A %A %A" s sorted distinct result
+    result
+
 let solve =
 
-    let lines = Common.getSampleDataAsArray 2022 06
-    // let lines = Common.getChallengeDataAsArray 2022 06
+    let markerChars = 14
+
+    // let lines = Common.getSampleData 2022 06
+    let lines = Common.getChallengeData 2022 06
     printfn "%A" lines
+
+    let testLines = [markerChars..lines.Length] |> List.map (fun len -> lines[0..len-1])
+    let tuples = testLines |> List.map (fun s -> (s, s[s.Length - markerChars..]))
+
+    let firstMatch = tuples |> List.tryFind (fun t -> isMarker (snd t))
+    printfn "%A" firstMatch
+    match firstMatch with
+        | None -> failwith("no match found")
+        | Some (line, marker) -> printfn "Found line %s with marker %s; length of line: %d" line marker line.Length 
