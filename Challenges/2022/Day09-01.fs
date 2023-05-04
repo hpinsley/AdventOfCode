907096c59504490291a80ea8c09958ce033cadee
Howard Pinsley <howard.pinsley@blackstone.com>
2023-05-04 07:51:08 -0400

    Set up problem 9

---
+++ Challenges/2022/Day09-01.fs
@@ -0,0 +1,14 @@
+﻿module Year2022Day09_Part1
+
+open System
+open System.IO
+open Common
+open System.Text.RegularExpressions
+
+
+let solve =
+    let lines = Common.getSampleDataAsArray 2022 09
+    // let lines = Common.getChallengeDataAsArray 2022 09
+
+    printfn "%A" lines
+
