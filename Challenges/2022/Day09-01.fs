a9ce57d1ac42712d096a00a37f6396a6be0bfa0f
Howard Pinsley <howard.pinsley@blackstone.com>
2023-05-06 19:39:53 -0400

    Day 9 Part 1 done

--- Challenges/2022/Day09-01.fs
+++ Challenges/2022/Day09-01.fs
@@ -22,7 +22,52 @@ let letterToDirection (letter:char): Direction =
 let lineToInstruction (line:string) : MoveInstruction =
     match line with
         | Common.ParseRegex "(.*) (.*)" [direction; steps] -> Move (letterToDirection direction[0], int(steps))
-    
+
+type State = {
+    headPosition: int * int
+    tailPosition: int * int
+    tailVisits: Set<int * int>
+}
+
+
+let moveOneStep (direction:Direction) (state:State) : State =
+    let (r, c) = state.headPosition
+
+    let afterHeadMoves = { state with headPosition =
+                                    match direction with
+                                        | Up -> (r - 1,c)
+                                        | Down -> (r + 1, c)
+                                        | Right -> (r, c + 1)
+                                        | Left -> (r, c - 1)
+                         }
+
+    // If the tail is not adjacent to head we have to move it
+    let { State.headPosition = (headRow,headCol); State.tailPosition = (tailRow, tailCol) } = afterHeadMoves
+    let rowDiff = headRow - tailRow
+    let colDiff = headCol - tailCol
+
+    if (abs rowDiff <= 1 && abs colDiff <= 1)
+    then // Adjacent to head; tail does not need to move
+        afterHeadMoves
+    else // Tail always moves one step in the correct directions to maintain adjacency
+        let row = tailRow + sign rowDiff
+        let col = tailCol + sign colDiff
+        { afterHeadMoves with 
+            tailPosition = (row, col);
+            tailVisits = Set.add (row, col) afterHeadMoves.tailVisits
+        }
+
+let rec processInstruction (state: State) (instruction:MoveInstruction) : State =
+    let (direction, steps) = match instruction with
+                                | Move (d, s) -> (d, s)
+
+    if (steps = 0)
+    then
+        state
+    else
+        let stepState = moveOneStep direction state         // Move only once
+        let nextInstruction = Move (direction, steps - 1)   // Recurse with one less step
+        processInstruction stepState nextInstruction
 
 let solve =
     // let lines = Common.getSampleDataAsArray 2022 09
@@ -34,4 +79,17 @@ let solve =
 
     for p in paired do
         printfn "%A => %A" (fst p) (snd p)
+
+    let startAt = (0,0)
+
+    let initialState = {
+        headPosition = startAt
+        tailPosition = startAt
+        tailVisits = Set.ofArray [| startAt|]
+    }
     
+    printfn "Initial state:\n%A" initialState
+    let finalState = moves |> Array.fold processInstruction initialState
+    printfn "Final state:\n%A" finalState
+
+    printfn "Part 1: %d" finalState.tailVisits.Count
