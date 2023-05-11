828b80029ab40eae431da53823dda396cd09a1a1
Howard Pinsley <howard.pinsley@blackstone.com>
2023-05-11 06:55:13 -0400

    Day 11 Complete - 2 stars

--- Challenges/2022/Day11-01.fs
+++ Challenges/2022/Day11-01.fs
@@ -6,23 +6,23 @@ open Common
 open System.Text.RegularExpressions
 open Microsoft.FSharp.Core.Operators.Checked
 
-type WorryFunc = int -> int
+type WorryFunc = int64 -> int64
 
 type Monkey =
     {
-        MonkeyNumber: int
-        ItemList: int list
+        MonkeyNumber: int64
+        ItemList: int64 list
         Operation: string
         Operand: string;
         OperationFunc: WorryFunc
-        Test: int -> bool
+        Test: int64 -> bool
         TrueTarget: int
         FalseTarget: int
-        InspectionCount: int
-        Divisor: int
+        InspectionCount: int64
+        Divisor: int64
     }
 
-let makeMonkey (monkeyNumber:int) : Monkey =
+let makeMonkey (monkeyNumber:int64) : Monkey =
     {
         MonkeyNumber = monkeyNumber
         ItemList = []
@@ -36,11 +36,12 @@ let makeMonkey (monkeyNumber:int) : Monkey =
         Divisor = 1
     }
 
-let parseOperation (operator:string) (operand:string) (divisor:int): WorryFunc =
+let parseOperation (operator:string) (operand:string) (divisor:int64): WorryFunc =
     let fetchOperand = match operand with
                         | "old" -> id
-                        | _ as num -> fun _ -> int num
+                        | _ as num -> fun _ -> int64 num
 
+    // 96140
     match operator with
         | "*" -> fun old -> ((old) * ((fetchOperand old))) % divisor
         | "+" -> fun old -> ((old) + ((fetchOperand old))) % divisor
@@ -55,12 +56,12 @@ let parseLines (lines:string[]) : Monkey[] =
             | ParseRegex "Monkey (.*):" [monkeyNumber] ->
                 if (Option.isSome currentMonkey) then
                     monkeyList <- Option.get currentMonkey :: monkeyList
-                currentMonkey <- makeMonkey (int monkeyNumber) |> Some
+                currentMonkey <- makeMonkey (int64 monkeyNumber) |> Some
         
             | ParseRegex "Starting items: (.*)" [itemList] ->
                 let monkey = Option.get currentMonkey
                 currentMonkey <- { monkey with 
-                                        ItemList = itemList.Split ", " |> Array.map int |> List.ofArray
+                                        ItemList = itemList.Split ", " |> Array.map int64 |> List.ofArray
                                     } |> Some
             | ParseRegex "Operation: new = old (.) (.*)" [operation; operand] ->
                 let monkey = Option.get currentMonkey
@@ -68,8 +69,8 @@ let parseLines (lines:string[]) : Monkey[] =
         
             | ParseRegex "Test: divisible by (.*)" [divisor] ->
                 let monkey = Option.get currentMonkey
-                let d = int divisor
-                let f = fun (n:int) -> n % d = 0
+                let d = int64 divisor
+                let f = fun (n:int64) -> n % d = 0
 
                 currentMonkey <- { monkey with 
                                         Test = f;
@@ -93,25 +94,25 @@ let parseLines (lines:string[]) : Monkey[] =
         monkeyList <- Option.get currentMonkey :: monkeyList
 
     let monkeyArray = monkeyList |> List.rev |> Array.ofList
-    let productOfDivisors = monkeyArray |> Array.fold (fun p m -> p * m.Divisor) 1
+    let productOfDivisors = monkeyArray |> Array.fold (fun p m -> p * m.Divisor) 1L
     let withOperatorFuncs = monkeyArray |> Array.map (fun m -> { m with OperationFunc = parseOperation m.Operation m.Operand productOfDivisors })
     withOperatorFuncs
 
-let playOneRound (postInspectionDivFactor:int) (monkeys:Monkey[]) : unit =
+let playOneRound (postInspectionDivFactor:int64) (monkeys:Monkey[]) : unit =
     for i = 0 to monkeys.Length - 1 do
         let monkey = monkeys[i]
         let toInspect = monkey.ItemList.Length
 
         for item in monkey.ItemList do
             let worry = (monkey.OperationFunc item) / postInspectionDivFactor
-            printfn $"Monkey {i} worry level is {worry}"
+            //printfn $"Monkey {i} worry level is {worry}"
 
             let target = if (monkey.Test worry) then monkey.TrueTarget else monkey.FalseTarget
             let targetMonkey = monkeys[target]
             monkeys[target] <- { targetMonkey with ItemList = List.append targetMonkey.ItemList [worry] }
         
         monkeys[i] <- { monkey with 
-                            InspectionCount = monkey.InspectionCount + int toInspect;
+                            InspectionCount = monkey.InspectionCount + int64 toInspect;
                             ItemList = []
                       } 
 
@@ -120,17 +121,17 @@ let computeMonkeyBusiness (monkeys:Monkey[]) : unit =
                 monkeys
                     |> Array.sortByDescending (fun m -> m.InspectionCount)
                     |> Array.take 2
-                    |> Array.fold (fun product monkey -> product * monkey.InspectionCount) 1 
+                    |> Array.fold (fun product monkey -> product * monkey.InspectionCount) 1L 
 
         printfn "MonkeyBusiness = %d" monkeyBusiness    
 
 let solve =
-    let lines = Common.getSampleDataAsArray 2022 11
-    //let lines = Common.getChallengeDataAsArray 2022 11
+    // let lines = Common.getSampleDataAsArray 2022 11
+    let lines = Common.getChallengeDataAsArray 2022 11
     //for line in lines do
     //    printfn "%s" line
 
-    let roundsToPlay = 20
+    let roundsToPlay = 10000
 
     let worryDivisor = 1
 
@@ -143,5 +144,5 @@ let solve =
     //for m in monkeys do
     //    printfn "Divisor: %d Monkey %d: Inspection count: %d" worryDivisor m.MonkeyNumber m.InspectionCount
 
-    printfn "%A" monkeys
+    // printfn "%A" monkeys
     computeMonkeyBusiness monkeys
