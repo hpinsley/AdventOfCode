77e8e304237885e900d259b751410544da09f160
Howard Pinsley <howard.pinsley@blackstone.com>
2023-05-11 06:58:39 -0400

    Day 12 setup

---
+++ Challenges/2022/Day12-01.fs
@@ -0,0 +1,14 @@
+﻿module Year2022Day12_Part1
+
+open System
+open System.IO
+open Common
+open System.Text.RegularExpressions
+open Microsoft.FSharp.Core.Operators.Checked
+
+let solve =
+    let lines = Common.getSampleDataAsArray 2022 12
+    // let lines = Common.getChallengeDataAsArray 2022 12
+    for line in lines do
+        printfn "%s" line
+
