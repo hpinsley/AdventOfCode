5e609c6a10d367b7b4dad3fedc46b3687a575d23
Howard Pinsley <howard.pinsley@blackstone.com>
2023-05-11 18:47:18 -0400

    Day 12 parse input

--- Challenges/2022/Day12-01.fs
+++ Challenges/2022/Day12-01.fs
@@ -6,9 +6,48 @@ open Common
 open System.Text.RegularExpressions
 open Microsoft.FSharp.Core.Operators.Checked
 
+type HeightMap = 
+    {
+        Heights: char[,];
+        StartLocation: int * int;
+        EndLocation: int * int;
+    }
+
+let getHeightMap (lines:string[]) : HeightMap =
+    let rows = lines.Length
+    let cols = lines[0].Length
+
+    let grid = Array2D.init rows cols (fun r c -> lines[r][c])
+    let mutable startLocation = (0, 0)
+    let mutable endLocation = (0, 0)
+
+    grid |> Array2D.iteri (fun r c character ->
+                                match character with
+                                    | 'S' -> grid[r,c] <- 'a'
+                                             startLocation <- (r,c)
+                                    | 'E' -> grid[r,c] <- 'z'
+                                             endLocation <- (r,c)
+                                    | _ -> ()
+                          )
+    let heights = {
+        Heights = grid
+        StartLocation = startLocation
+        EndLocation = endLocation
+    }
+    
+    heights
+
 let solve =
     let lines = Common.getSampleDataAsArray 2022 12
-    // let lines = Common.getChallengeDataAsArray 2022 12
+    //let lines = Common.getChallengeDataAsArray 2022 12
     for line in lines do
         printfn "%s" line
 
+    printfn ""
+
+    let heightMap = getHeightMap lines
+    printGrid heightMap.Heights id
+
+    printfn ""
+
+    printfn "%A" heightMap
