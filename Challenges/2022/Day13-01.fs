bd0c6a1a5e28ec5f39e39cfea0a7a2c14705e611
Howard Pinsley <howard.pinsley@blackstone.com>
2023-05-17 08:05:42 -0400

    Set up day 13

---
+++ Challenges/2022/Day13-01.fs
@@ -0,0 +1,14 @@
+﻿module Year2022Day13_Part1
+
+open System
+open System.IO
+open Common
+open System.Text.RegularExpressions
+open Microsoft.FSharp.Core.Operators.Checked
+open System.Collections.Generic
+
+let solve =
+    let lines = Common.getSampleDataAsArray 2022 13
+    // let lines = Common.getChallengeDataAsArray 2022 13
+    for line in lines do
+        printfn "%s" line
