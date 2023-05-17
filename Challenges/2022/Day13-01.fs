913a5bfa7e580c032d03a6b1d46424bf497851d6
Howard Pinsley <howard.pinsley@blackstone.com>
2023-05-17 08:26:53 -0400

    WIP

--- Challenges/2022/Day13-01.fs
+++ Challenges/2022/Day13-01.fs
@@ -7,8 +7,20 @@ open System.Text.RegularExpressions
 open Microsoft.FSharp.Core.Operators.Checked
 open System.Collections.Generic
 
+type PacketItem =
+    | Value of int
+    | Nested of PacketItem list
+
+type Packet = PacketItem list
+
+let parsePacket (line:string) : Packet =
+    [Value 3]
+
 let solve =
     let lines = Common.getSampleDataAsArray 2022 13
     // let lines = Common.getChallengeDataAsArray 2022 13
     for line in lines do
-        printfn "%s" line
+        let packet = parsePacket line
+        printfn "%s = %A" line packet
+
+
