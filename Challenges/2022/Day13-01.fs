1753a2998db0994de7ba31233a0a13e580e64116
Howard Pinsley <howard.pinsley@blackstone.com>
2023-05-18 08:28:09 -0400

    More parsing

--- Challenges/2022/Day13-01.fs
+++ Challenges/2022/Day13-01.fs
@@ -17,7 +17,7 @@ let LBracket = '['
 let RBracket = ']'
 let Comma = ','
 
-let rec parsePacketItem (buffer:char[]) (startAt:int) : (Option<PacketItem> * int) =
+let rec parseNumberPacketItem (buffer:char[]) (startAt:int) : (Option<PacketItem> * int) =
     let maxIndex = buffer.Length - 1
     let mutable intBuffer:char list = []
     let mutable item:PacketItem option = None
@@ -27,7 +27,7 @@ let rec parsePacketItem (buffer:char[]) (startAt:int) : (Option<PacketItem> * in
     while (Option.isNone item && p < maxIndex) do
         p <- p + 1
         let c = buffer[p]
-        if (c >= '0' && c <= '9') then  
+        if (Char.IsDigit(c)) then  
             intBuffer <- List.append intBuffer [c]
         else
             let s = new string [| for c in intBuffer -> c|]
@@ -35,6 +35,55 @@ let rec parsePacketItem (buffer:char[]) (startAt:int) : (Option<PacketItem> * in
             item <- Value v |> Some
 
     (item, p)
+
+let rec parseListPacketItem (buffer:char[]) (startAt:int) (itemParseFunc:char[] -> int -> (Option<PacketItem> * int)): (Option<PacketItem> * int) =
+    let mutable packetItems:PacketItem list = []
+
+    let c = buffer[startAt]
+    if (c <> LBracket)
+    then
+        failwith "Expected LBracket"
+
+    let mutable p = startAt + 1
+    let mutable isDone = false
+
+    while (not isDone) do
+        let (item, newPos) = itemParseFunc buffer p
+        match item with
+            | Some packetItem ->
+                packetItems <- List.append packetItems [packetItem]
+                p <- newPos
+                if (buffer[p] = Comma)
+                then
+                    p <- p + 1
+                elif (buffer[p] = RBracket)
+                then
+                    isDone <- true
+                    p <- p + 1
+                else
+                    failwith "Expected closing bracket"
+            | None -> 
+                p <- newPos
+                let bracket = buffer[p]
+                if (bracket <> RBracket)
+                then
+                    failwith "Expected closing right bracket after no item returned"
+                
+                p <- p + 1
+                isDone <- true
+
+    (Some (Nested packetItems), p)
+
+let rec parsePacketItem (buffer:char[]) (startAt:int) : (Option<PacketItem> * int) =
+    let c = buffer[startAt]
+    if (Char.IsDigit(c))
+    then
+        parseNumberPacketItem buffer startAt
+    else if (c = RBracket)
+    then
+        (None, startAt)
+    else
+        parseListPacketItem buffer startAt parsePacketItem
 
 
 let parsePacket (line:string) : Packet =
@@ -72,6 +121,8 @@ let solve =
     //    let packet = parsePacket line
     //    printfn "%s = %A" line packet
     
-    let result = parsePacket("[17,18]")
+    //let result = parsePacket("[17,18]")
+    //let result = parsePacket("[[18,19]]")
+    let result = parsePacket("[[]]")
     printfn "%A" result
 
