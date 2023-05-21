74c2032bd137d57dbb75d678aad46a3d879c2e2b
Howard Pinsley <howard.pinsley@blackstone.com>
2023-05-21 17:05:11 -0400

    Day 13 Part 1 solved.

--- Challenges/2022/Day13-01.fs
+++ Challenges/2022/Day13-01.fs
@@ -11,8 +11,6 @@ type PacketItem =
     | Value of int
     | Nested of PacketItem list
 
-type Packet = PacketItem list
-
 let LBracket = '['
 let RBracket = ']'
 let Comma = ','
@@ -86,33 +84,14 @@ let rec parsePacketItem (buffer:char[]) (startAt:int) : (Option<PacketItem> * in
         parseListPacketItem buffer startAt parsePacketItem
 
 
-let parsePacket (line:string) : Packet =
+let parsePacket (line:string) : PacketItem =
     let chars = Array.ofSeq line
-    if (chars[0] <> LBracket || chars[chars.Length - 1] <> RBracket)
-    then
-        failwith "Packets must be surrounded by ()"
-
-    let mutable packet:PacketItem list = []
-
-    let mutable p = 1
-    let mutable isDone = false
-
-    while (not isDone) do
-        let (item, newPos) = parsePacketItem chars p
-        match item with
-            | Some packetItem ->
-                packet <- List.append packet [packetItem]
-                p <- newPos
-                if (chars[p] = Comma)
-                then
-                    p <- p + 1
-                elif (chars[p] = RBracket)
-                then
-                    isDone <- true
-
-            | None -> isDone <- true
-
-    packet
+    let (piOpt, finish) = parsePacketItem chars 0
+    match piOpt with
+        | Some pi -> 
+            //printfn "Finished at %d" finish
+            pi
+        | None -> failwith "No packet"
 
 let testAllLines (lines:string[]) : unit =
     for line in lines do
@@ -123,7 +102,7 @@ let testAllLines (lines:string[]) : unit =
         printfn "%s = %A" line packet
 
 // Pair up the valid lines, parse them and index the pairs starting at 1
-let correlatePairs (lines:string[]) : seq<int * (Packet * Packet)> =
+let correlatePairs (lines:string[]) : seq<int * (PacketItem * PacketItem)> =
     let numberedLines =
         lines
             |> Array.filter (fun line -> line.Length > 1)
@@ -132,13 +111,63 @@ let correlatePairs (lines:string[]) : seq<int * (Packet * Packet)> =
     let right = numberedLines |> Seq.filter (fun (i, line) -> i = 1) |> Seq.map (snd >> parsePacket)
     Seq.zip left right |> Seq.mapi (fun i pair -> (i+1, pair))
 
+let rec comparePacketItems (left:PacketItem) (right:PacketItem) : int =
+    match left with
+        
+        | Value lValue ->
+            match right with
+                | Value rValue -> 
+                    if (lValue < rValue)
+                    then
+                        -1
+                    elif (lValue > rValue)
+                    then
+                        1
+                    else
+                        0
+                | Nested _ -> comparePacketItems (Nested [Value lValue]) right
+
+        | Nested leftList ->
+            match right with
+                | Value rValue ->
+                    comparePacketItems left (Nested [Value rValue])
+                | Nested rightList ->
+                    printfn "Comparing left list of %d items and right list of %d items" leftList.Length rightList.Length
+                    match leftList with
+                        | [] ->
+                            match rightList with
+                                | [] -> 0
+                                | head :: tail -> -1
+                        | leftItem :: leftRemaining ->
+                            match rightList with
+                                | [] -> 1
+                                | rightItem :: rightRemaining ->
+                                    let result = comparePacketItems leftItem rightItem
+                                    match result with
+                                        | -1 | 1 -> result
+                                        | 0 -> comparePacketItems (Nested leftRemaining) (Nested rightRemaining)
+
+                        
+
+let comparePair ((indexNo:int), (left:PacketItem, right:PacketItem)) =
+    let result = comparePacketItems left right
+    printfn "Comparing index %d: Result: %d" indexNo result
+    if (result = -1)
+    then
+        indexNo
+    else
+        0
+
 let solve =
-    // let lines = Common.getSampleDataAsArray 2022 13
+    ////let lines = Common.getSampleDataAsArray 2022 13
     let lines = Common.getChallengeDataAsArray 2022 13
     // testAllLines lines
 
-    let pairs = correlatePairs lines
+    let pairs = correlatePairs lines |> Array.ofSeq
     for (index, (left, right)) in pairs do
-        printfn "%d: %A %A" index left right
-    ()
+        printfn "%d: Left:%A Right: %A" index left right
+    
+    let result = pairs |> Seq.map comparePair |> Seq.sum
+    printfn "Final result is %d" result
 
+    ()
