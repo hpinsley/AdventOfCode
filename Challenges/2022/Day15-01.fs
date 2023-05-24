9859f6a944f3aa46cb990f02a7e4d43a564edc28
Howard Pinsley <howard.pinsley@blackstone.com>
2023-05-24 07:45:34 -0400

    Parsing complete

--- Challenges/2022/Day15-01.fs
+++ Challenges/2022/Day15-01.fs
@@ -7,8 +7,51 @@ open System.Text.RegularExpressions
 open Microsoft.FSharp.Core.Operators.Checked
 open System.Collections.Generic
 
+type Point = int * int
+type Reading = {
+    sensor: Point
+    beacon: Point
+    distance: int
+}
+
+let GetX = fst
+let GetY = snd
+
+let getDistance (p1:Point) (p2:Point) : int =
+    let x1 = GetX p1
+    let x2 = GetX p2
+    let y1 = GetY p1
+    let y2 = GetY p2
+
+    Math.Abs(x2 - x1) + Math.Abs(y2 - y1)
+
+let parseLine (line:string) : Reading =
+    let m = Regex("Sensor at x=([-0123456789]+), y=([-0123456789]+): closest beacon is at x=([-0123456789]+), y=([-0123456789]+)").Match(line)
+    
+    let values = seq { 1 .. 4 } 
+                    |> Seq.map (fun index -> int m.Groups[index].Value)
+                    |> Array.ofSeq
+
+    let sensor = (values[0], values[1]);
+    let beacon = (values[2], values[3])
+
+    let (reading:Reading) = {
+
+        sensor = sensor;
+        beacon = beacon;
+        distance = getDistance sensor beacon;
+    }
+
+    reading
+
 let solve =
-    let lines = Common.getSampleDataAsArray 2022 15
-    // let lines = Common.getChallengeDataAsArray 2022 15
+    // let lines = Common.getSampleDataAsArray 2022 15
+    let lines = Common.getChallengeDataAsArray 2022 15
     printAllLines lines
+
+    let readings = lines |> Seq.map parseLine
+    printfn "%A" readings
+
+
+
     ()
