502a532fb36ad9731ffe924f8b92c5ac24376762
Howard Pinsley <howard.pinsley@blackstone.com>
2023-05-28 11:01:41 -0400

    Parse string input

--- Challenges/2022/Day16-01.fs
+++ Challenges/2022/Day16-01.fs
@@ -7,9 +7,37 @@ open System.Text.RegularExpressions
 open Microsoft.FSharp.Core.Operators.Checked
 open System.Collections.Generic
 
+type ValveInfo =
+    {
+        valveName: string;
+        flowRate: int;
+        leadsTo: string[]
+    }
+
+let parseLine (line:string) : ValveInfo =
+    let sample = "Valve EB has flow rate=7; tunnels lead to valves IF, NH, AD, VI, DQ"
+    let pattern = "Valve (..) has flow rate=(\d+); tunnel(s)? lead(s)? to valve(s)? (.*)"
+    let m = Regex(pattern).Match(line)
+    if (not m.Success) then failwith "regex"
+
+    let targetList = (m.Groups[6].Value).Replace(" ", "")
+    let targets = targetList.Split(',')
+    let info = {
+        valveName = m.Groups[1].Value;
+        flowRate = int m.Groups[2].Value
+        leadsTo = targets
+    }
+    info
+
+let printValveInfo (valves:ValveInfo[]) =
+    for v in valves do
+        printfn "Valve %s with flow %d links to %A" v.valveName v.flowRate v.leadsTo
+
 let solve =
     let lines = Common.getSampleDataAsArray 2022 16
     // let lines = Common.getChallengeDataAsArray 2022 16
-
     printAllLines lines
+
+    let valveInfo = lines |> Array.map parseLine
+    printValveInfo valveInfo
     ()
