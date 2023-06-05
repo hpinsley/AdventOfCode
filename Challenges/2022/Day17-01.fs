52bd01f360c125c041b22750b007cfc6ac5f4d41
Howard Pinsley <howard.pinsley@blackstone.com>
2023-06-05 18:45:15 -0400

    New types Rock and Cave

--- Challenges/2022/Day17-01.fs
+++ Challenges/2022/Day17-01.fs
@@ -7,14 +7,27 @@ open System.Text.RegularExpressions
 open Microsoft.FSharp.Core.Operators.Checked
 open System.Collections.Generic
 
-type Rock =
+type RockTemplate =
     {
         rows: int;
         cols: int;
         occupies: (int * int) Set;      // Let's use rows and cols not x and y
     }
 
-let RockTemplates = [|
+type Rock =
+    {
+        rows: int;
+        cols: int;
+        mutable occupies: (int * int) Set;      // Let's use rows and cols not x and y
+    }
+
+type Cave =
+    {
+        maxHeight: int;
+        rocks: Rock list
+    }
+
+let rockTemplates = [|
                         { rows = 1; cols = 4; 
                             occupies = Set.ofList 
                                         [   
@@ -55,7 +68,7 @@ let RockTemplates = [|
                         }                        
                     |]
 
-let printRockTemplate (rock:Rock) : unit =
+let printRockTemplate (rock:RockTemplate) : unit =
     let grid = Array2D.init rock.rows rock.cols (fun row col -> if Set.contains (row,col) rock.occupies then '#' else '.') 
     printGrid grid id
 
@@ -69,8 +82,8 @@ let getWindDirection (line:string) : int[] =
 
 
 let solve =
-    // let lines = Common.getSampleDataAsArray 2022 17
-    let lines = Common.getChallengeDataAsArray 2022 17
+    let lines = Common.getSampleDataAsArray 2022 17
+    // let lines = Common.getChallengeDataAsArray 2022 17
 
     printAllLines lines
     printfn "There are %d lines in the input and the first one is %d chars" lines.Length lines[0].Length
@@ -79,17 +92,22 @@ let solve =
     printfn "%A (of length %d)" windDirections windDirections.Length
 
     let windStream = Seq.initInfinite (fun i -> windDirections[i % windDirections.Length])
-    let rockStream = Seq.initInfinite (fun i -> RockTemplates[i % RockTemplates.Length])
+    let rockStream = Seq.initInfinite (fun i -> rockTemplates[i % rockTemplates.Length])
     let windEnumerator = windStream.GetEnumerator()
     let rockEnumerator = rockStream.GetEnumerator()
 
-    while rockEnumerator.MoveNext() do
-        let mutable rock = rockEnumerator.Current
-        printRockTemplate rock
-        printfn ""
+    //while rockEnumerator.MoveNext() do
+    //    let mutable rock = rockEnumerator.Current
+    //    printRockTemplate rock
+    //    printfn ""
 
-    printfn "We have %d rock templates" RockTemplates.Length
-    for rock in RockTemplates do
-        printfn ""
-        printRockTemplate rock
+    printfn "We have %d rock templates" rockTemplates.Length
+
+    let(cave:Cave) = {
+        maxHeight = 0;
+        rocks = []
+    }
+
+    printfn "Cave: %A" cave
+
     ()
