72f6166fcbabb21daaa3fbafda873be2a3acb018
Howard Pinsley <howard.pinsley@blackstone.com>
2023-06-05 19:02:07 -0400

    Position the rock to start falling

--- Challenges/2022/Day17-01.fs
+++ Challenges/2022/Day17-01.fs
@@ -27,46 +27,46 @@ type Cave =
         rocks: Rock list
     }
 
-let rockTemplates = [|
-                        { rows = 1; cols = 4; 
-                            occupies = Set.ofList 
-                                        [   
-                                            (0,0);  (0,1);  (0,2);  (0,3)
-                                        ]
-                        }
+let (rockTemplates:RockTemplate[]) = [|
+                            { rows = 1; cols = 4; 
+                                occupies = Set.ofList 
+                                            [   
+                                                (0,0);  (0,1);  (0,2);  (0,3)
+                                            ]
+                            }
 
-                        { rows = 3; cols = 3; occupies = Set.ofList
-                                        [
-                                                    (0,1);
-                                            (1,0);  (1,1);  (1,2);
-                                                    (2,1)
-                                        ]  
-                        }                        
+                            { rows = 3; cols = 3; occupies = Set.ofList
+                                            [
+                                                        (0,1);
+                                                (1,0);  (1,1);  (1,2);
+                                                        (2,1)
+                                            ]  
+                            }                        
  
-                        { rows = 3; cols = 3; occupies = Set.ofList
-                                        [
-                                                            (0,2);
-                                                            (1,2);
-                                            (2,0);  (2,1);  (2,2);
-                                        ]  
-                        }                        
-                        { rows = 4; cols = 1; 
-                            occupies = Set.ofList 
-                                        [   
-                                            (0,0);
-                                            (1,0);
-                                            (2,0);
-                                            (3,0);
-                                        ]
-                        }
+                            { rows = 3; cols = 3; occupies = Set.ofList
+                                            [
+                                                                (0,2);
+                                                                (1,2);
+                                                (2,0);  (2,1);  (2,2);
+                                            ]  
+                            }                        
+                            { rows = 4; cols = 1; 
+                                occupies = Set.ofList 
+                                            [   
+                                                (0,0);
+                                                (1,0);
+                                                (2,0);
+                                                (3,0);
+                                            ]
+                            }
 
-                        { rows = 2; cols = 2; occupies = Set.ofList
-                                        [
-                                            (0,0);  (0,1);
-                                            (1,0);  (1,1);
-                                        ]  
-                        }                        
-                    |]
+                            { rows = 2; cols = 2; occupies = Set.ofList
+                                            [
+                                                (0,0);  (0,1);
+                                                (1,0);  (1,1);
+                                            ]  
+                            }                        
+                        |]
 
 let printRockTemplate (rock:RockTemplate) : unit =
     let grid = Array2D.init rock.rows rock.cols (fun row col -> if Set.contains (row,col) rock.occupies then '#' else '.') 
@@ -80,6 +80,25 @@ let getWindDirection (line:string) : int[] =
                                 | _ -> failwith "unknown direction")
         |> Array.ofSeq
 
+let solvePart1 (cave:Cave) (windEnumerator:IEnumerator<int>) (rockTemplateEnumerator:IEnumerator<RockTemplate>) : unit =
+    let maxRocksToFall = 1
+    for r in seq { 0 .. maxRocksToFall - 1} do
+        printfn "Dropping rock %d" r
+        rockTemplateEnumerator.MoveNext() |> ignore
+        let template = rockTemplateEnumerator.Current
+        let startingRow = cave.maxHeight + 4
+        let startingCol = 3
+
+        let (rock:Rock) = {
+            rows = template.rows
+            cols = template.cols
+            occupies = template.occupies
+                        |> Seq.map (fun (row,col) -> (row + startingRow, col + startingCol))
+                        |> Set.ofSeq
+        }
+        printfn "Rock %d = %A" r rock
+        ()
+
 
 let solve =
     let lines = Common.getSampleDataAsArray 2022 17
@@ -92,11 +111,11 @@ let solve =
     printfn "%A (of length %d)" windDirections windDirections.Length
 
     let windStream = Seq.initInfinite (fun i -> windDirections[i % windDirections.Length])
-    let rockStream = Seq.initInfinite (fun i -> rockTemplates[i % rockTemplates.Length])
+    let rockTemplateStream = Seq.initInfinite (fun i -> rockTemplates[i % rockTemplates.Length])
     let windEnumerator = windStream.GetEnumerator()
-    let rockEnumerator = rockStream.GetEnumerator()
+    let rockTemplateEnumerator = rockTemplateStream.GetEnumerator()
 
-    //while rockEnumerator.MoveNext() do
+    //while rockTemplateEnumerator.MoveNext() do
     //    let mutable rock = rockEnumerator.Current
     //    printRockTemplate rock
     //    printfn ""
@@ -110,4 +129,5 @@ let solve =
 
     printfn "Cave: %A" cave
 
+    solvePart1 cave windEnumerator rockTemplateEnumerator
     ()
