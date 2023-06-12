25c12e6079ca6bc6f3c871e70f5de8d67106f580
Howard Pinsley <howard.pinsley@blackstone.com>
2023-06-12 18:15:38 -0400

    Setup day 11

---
+++ Challenges/2022/Day18-01.fs
@@ -0,0 +1,26 @@
+﻿module Year2022Day18_Part1
+
+open System
+open System.IO
+open Common
+open System.Text.RegularExpressions
+open Microsoft.FSharp.Core.Operators.Checked
+open System.Collections.Generic
+
+type Point = int * int * int
+
+let getOriginPoints (lines:string[]) : Point[] =
+    lines |>
+        Array.map (fun s -> 
+                    let split = s.Split(',')
+                    (int split[0], int split[1], int split[2])
+                )
+
+let solve =
+    let lines = Common.getSampleDataAsArray 2022 18
+    // let lines = Common.getChallengeDataAsArray 2022 18
+
+    printAllLines lines
+    let points = getOriginPoints lines
+    printfn "%A" points
+    ()
