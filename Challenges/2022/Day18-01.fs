8215bc5048bd67ea18f68120dcdefe8456ebda97
Howard Pinsley <howard.pinsley@blackstone.com>
2023-06-12 19:13:59 -0400

    wip

--- Challenges/2022/Day18-01.fs
+++ Challenges/2022/Day18-01.fs
@@ -7,20 +7,87 @@ open System.Text.RegularExpressions
 open Microsoft.FSharp.Core.Operators.Checked
 open System.Collections.Generic
 
-type Point = int * int * int
+type Point = {
+    x: int
+    y: int
+    z: int
+}
+
+type Side = Point[]
+
+type Cube = {
+    allSides: Set<Point>[];
+}
+
+let generateCube (origin:Point): Cube =
+    let bottom  = [|    { x = origin.x; y = origin.y; z = origin.z }; 
+                    { x = origin.x; y = origin.y + 1; z = origin.z };
+                    { x = origin.x + 1; y = origin.y; z = origin.z };
+                    { x = origin.x + 1; y = origin.y + 1; z = origin.z }
+                |]
+
+    let top  = [|       { x = origin.x; y = origin.y; z = origin.z + 1}; 
+                    { x = origin.x; y = origin.y + 1; z = origin.z + 1 };
+                    { x = origin.x + 1; y = origin.y; z = origin.z + 1};
+                    { x = origin.x + 1; y = origin.y + 1; z = origin.z + 1 }
+                |]
+
+    let left  = [|      { x = origin.x; y = origin.y; z = origin.z}; 
+                    { x = origin.x; y = origin.y + 1; z = origin.z };
+                    { x = origin.x; y = origin.y; z = origin.z + 1};
+                    { x = origin.x; y = origin.y + 1; z = origin.z + 1 }
+                |]
+    let right  = [|      { x = origin.x + 1; y = origin.y; z = origin.z}; 
+                    { x = origin.x + 1; y = origin.y + 1; z = origin.z };
+                    { x = origin.x + 1; y = origin.y; z = origin.z + 1};
+                    { x = origin.x + 1; y = origin.y + 1; z = origin.z + 1 }
+                |]
+
+    let back  = [|      { x = origin.x; y = origin.y + 1; z = origin.z}; 
+                    { x = origin.x + 1; y = origin.y + 1; z = origin.z };
+                    { x = origin.x; y = origin.y + 1; z = origin.z };
+                    { x = origin.x + 1; y = origin.y + 1; z = origin.z + 1 }
+                |]
+    let front  = [|     { x = origin.x; y = origin.y; z = origin.z}; 
+                    { x = origin.x + 1; y = origin.y; z = origin.z };
+                    { x = origin.x; y = origin.y; z = origin.z };
+                    { x = origin.x + 1; y = origin.y; z = origin.z + 1 }
+                |]
+
+    let sides = [|bottom; top; left; right; back; front |]
+    let sets = sides
+                |> Array.map (fun side -> Set.ofArray side)
+    let (cube:Cube) = {
+                        allSides = sets
+                        }
+
+    cube
+
+let getCommonSides (cube1: Cube, cube2: Cube) : Side[] =
+    START HERE
 
 let getOriginPoints (lines:string[]) : Point[] =
     lines |>
         Array.map (fun s -> 
                     let split = s.Split(',')
-                    (int split[0], int split[1], int split[2])
+                    {
+                        x = int split[0]
+                        y = int split[1]
+                        z = int split[2]
+                    }
                 )
 
 let solve =
-    let lines = Common.getSampleDataAsArray 2022 18
+    // let lines = Common.getSampleDataAsArray 2022 18
     // let lines = Common.getChallengeDataAsArray 2022 18
+    let lines = [| "1,1,1"; "2,1,1"|]
+    // printAllLines lines
+    let originPoints = getOriginPoints lines
+    printfn "\nCube located at point %A" originPoints[0]
+    printfn ""
+    let cubes = originPoints |> Array.map generateCube
 
-    printAllLines lines
-    let points = getOriginPoints lines
-    printfn "%A" points
+    printfn "%A" cubes
+
+    printfn "There are %d cubes to build" originPoints.Length
     ()
