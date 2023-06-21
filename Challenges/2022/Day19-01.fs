b6b6d21742f2ba68e32c0086e56eac0597bddd43
Howard Pinsley <howard.pinsley@blackstone.com>
2023-06-21 08:27:08 -0400

    WIP

--- Challenges/2022/Day19-01.fs
+++ Challenges/2022/Day19-01.fs
@@ -15,6 +15,13 @@ type Material =
     | Obsidian
     | Geode
 
+let getSuccessorMaterial (fromMaterial:Material) : Material option =
+    match fromMaterial with
+        | Ore -> Some Clay
+        | Clay -> Some Obsidian
+        | Obsidian -> Some Geode
+        | Geode -> None
+
 type RobotSpec = {
     manufactures: Material
     requires: Map<Material, int>
@@ -27,7 +34,7 @@ type Robot = {
 
 type BluePrint = {
     planNumber: int
-    robotSpecs: RobotSpec[]
+    robotSpecs: Map<Material, RobotSpec>
 }
 
 type State = {
@@ -64,29 +71,70 @@ let makeRobotSpec (m:Match) (robotType:Material) (matchBase:int) : RobotSpec =
     robotSpec
 
 let makeRobot (bluePrint:BluePrint) (creationMinute: int) (material:Material) =
-    let spec = bluePrint.robotSpecs |> Array.find (fun spec -> spec.manufactures = material)
+    let spec = bluePrint.robotSpecs[material]
     let robot = {
         spec = spec
         creationMinute = creationMinute
     }
     robot
 
-let createAltStates (bluePrint: BluePrint, rootState:State) : State list =
-    let materials = bluePrint.robotSpecs |> Array.map (fun s -> s.manufactures)
+// Create states with all possible robot spawning
+let rec createAltStates (bluePrint: BluePrint) (rootState:State) (materialOption:Material option) (robotCount:int): State list =
+    match materialOption with
+        | None -> []
+        | Some material ->
+            let requirements = bluePrint.robotSpecs[material].requires
+            let creationMinute = rootState.minute
 
-    for material in materials do
-        let spec = bluePrint.robotSpecs |> Array.find (fun s -> s.manufactures = material)
-        for createCount in seq { 0.. 3} do
+            let newInventory =
+                
+                rootState.inventory 
+                    |> Seq.map (fun kvp ->
+                                    let requiredAmount =
+                                        (Map.tryFind kvp.Key requirements)
+                                            |> Option.defaultValue 0
+                                            |> (*) robotCount
+
+                                    (kvp.Key, kvp.Value - requiredAmount)
+                                )
+                                |> Map.ofSeq
+
+            let canCreateRobots = newInventory
+                                    |> Map.values
+                                    |> Seq.exists (fun v -> v < 0)
+                                    |> not
+
+            let newStates =
+                if (canCreateRobots)
+                then
+                    if (robotCount > 0)
+                    then
+                        let newRobots =
+                            seq { 1 .. robotCount}
+                                |> Seq.map (fun _ -> makeRobot bluePrint creationMinute material)
+                                |> List.ofSeq
+
+                        let newState = { rootState with
+                                            robots = rootState.robots |> List.append newRobots
+                                            inventory = newInventory
+                                       }
+                    
+                        createAltStates bluePrint newState materialOption (robotCount + 1)
+                    else
+                        createAltStates bluePrint rootState materialOption (robotCount + 1)
+                        
+                else
+                    createAltStates bluePrint rootState (getSuccessorMaterial material) 0
             
-
-    []
-
+            rootState :: newStates    
+    
 let optimizeBlueprint (bluePrint:BluePrint) : unit =
     let robot = makeRobot bluePrint 0 Ore
     let robots = [ robot ]
 
     let state = {
-        inventory = Map.empty
+        inventory = [(Ore, 0); (Clay, 0); (Obsidian, 0); (Geode, 0)]
+                        |> Map.ofSeq
         robots = robots
         minute = 0
     }
@@ -96,7 +144,8 @@ let optimizeBlueprint (bluePrint:BluePrint) : unit =
 
     while (states.Count > 0) do
         let state = states.Dequeue()
-        let altStates = createAltStates(bluePrint, state)
+        let altStates = createAltStates bluePrint state (Some Ore) 0
+        printfn "altStates: %A" altStates
         ()
     ()
 
@@ -116,7 +165,12 @@ let parseLine (line:string) : BluePrint =
 
     let bluePrint = {
         planNumber = int m.Groups[1].Value
-        robotSpecs = [| oreRobot; clayRobot; obsidianRobot; geodeRobot |]
+        robotSpecs = [
+                        (Ore, oreRobot)
+                        (Clay, clayRobot)
+                        (Obsidian, obsidianRobot)
+                        (Geode, geodeRobot)
+                     ] |> Map.ofList
     }
 
     bluePrint
@@ -124,7 +178,9 @@ let parseLine (line:string) : BluePrint =
 let solve =
     let lines = Common.getSampleDataAsArray 2022 19
     // let lines = Common.getChallengeDataAsArray 2022 19
-    printAllLines lines
+    //printAllLines lines
     let plans = lines |> Array.map parseLine
-    printfn "%A" plans
+    let bluePrint = plans[0]
+    let result = optimizeBlueprint bluePrint
+    printfn "Result: %A" result
     ()
