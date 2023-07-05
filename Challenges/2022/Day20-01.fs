af8a04af8f9cf26109bc76f0fd8b6606c1a94218
Howard Pinsley <howard.pinsley@blackstone.com>
2023-07-05 19:26:50 -0400

    Construct the ring

--- Challenges/2022/Day20-01.fs
+++ Challenges/2022/Day20-01.fs
@@ -12,20 +12,74 @@ let getInputArray (lines:string[]) : int[] =
         |> Array.map parseInt
 
 type Cell (value:int) =
-    member this.v = value
-    member this.pred = this
-    member this.succ = this
+    let mutable pred = None
+    let mutable succ = None
+    let v = value
 
-let parseValuesIntoRing (values:int[]) : Cell =
-    let length = Array.length values
-    Cell(7)
+    member this.V with get() = v
 
+    member this.Pred
+            with
+                get() = pred |> Option.defaultValue (new Cell(-1))
+            and
+                set(newPred:Cell) = pred <- Some newPred
+
+    member this.Succ
+            with
+                get() = succ |> Option.defaultValue (new Cell(-1))
+            and
+                set(newSucc:Cell) = succ <- Some newSucc
+
+let parseValuesIntoRing (values:int[]) : Tuple<Cell,Cell[]> =
+    let valueList = List.ofArray values
+    let mutable firstCell = None
+    let mutable lastCell = None
+    let mutable pointers = []
+
+    let rec addToList (parent:Cell option) (v:int list) =
+        match v with
+            | head :: tail ->
+                let newCell = Cell(head)
+                pointers <- newCell :: pointers
+                match parent with
+                    | Some p ->
+                        p.Succ <- newCell
+                        newCell.Pred <- p
+                    | None -> ()
+                match firstCell with
+                    | None -> firstCell <- Some newCell
+                    | Some c -> ()
+
+                lastCell <- Some newCell
+                addToList (Some newCell) tail
+            | [] -> ()
+    
+    addToList None valueList
+
+    // Link the first and last to complete the ring and return the first
+    match firstCell with
+        | Some f ->
+                match lastCell with
+                    | Some last ->
+                        last.Succ <- f
+                        f.Pred <- last
+                        let p = pointers |> List.rev |> Array.ofList
+                        (f, p)
+                    | None -> failwith "No last cell"
+        | None -> failwith "No first cell set"
+
+let printRing (c:Cell) (ringSize:int) =
+    let mutable current = c
+    for _ in seq { 1 .. ringSize } do
+        printfn "Cell %d" current.V
+        current <- current.Succ
+                
 let solve =
     let lines = Common.getSampleDataAsArray 2022 20
     // let lines = Common.getChallengeDataAsArray 2022 20
     // printAllLines lines
     printfn "There are %d coordinates" lines.Length
     let values = getInputArray lines
-    let ring = parseValuesIntoRing values
-    ring.v <- 2
+    let (ring,pointers) = parseValuesIntoRing values
+    printRing ring values.Length
     ()
