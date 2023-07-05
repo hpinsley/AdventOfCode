8dfc65cbc2aafbe00ccbada5c42943fda89d991d
Howard Pinsley <howard.pinsley@blackstone.com>
2023-07-05 08:26:03 -0400

    WIP

--- Challenges/2022/Day20-01.fs
+++ Challenges/2022/Day20-01.fs
@@ -7,9 +7,25 @@ open System.Text.RegularExpressions
 open Microsoft.FSharp.Core.Operators.Checked
 open System.Collections.Generic
 
-    
+let getInputArray (lines:string[]) : int[] =
+    lines
+        |> Array.map parseInt
+
+type Cell (value:int) =
+    member this.v = value
+    member this.pred = this
+    member this.succ = this
+
+let parseValuesIntoRing (values:int[]) : Cell =
+    let length = Array.length values
+    Cell(7)
+
 let solve =
     let lines = Common.getSampleDataAsArray 2022 20
     // let lines = Common.getChallengeDataAsArray 2022 20
-    printAllLines lines
+    // printAllLines lines
+    printfn "There are %d coordinates" lines.Length
+    let values = getInputArray lines
+    let ring = parseValuesIntoRing values
+    ring.v <- 2
     ()
