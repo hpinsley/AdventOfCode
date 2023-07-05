9e2b157f191236f90e308ee36cbd4699480e32f2
Howard Pinsley <howard.pinsley@blackstone.com>
2023-07-05 07:38:16 -0400

    Setup day 20

---
+++ Challenges/2022/Day20-01.fs
@@ -0,0 +1,15 @@
+﻿module Year2022Day20_Part1
+
+open System
+open System.IO
+open Common
+open System.Text.RegularExpressions
+open Microsoft.FSharp.Core.Operators.Checked
+open System.Collections.Generic
+
+    
+let solve =
+    let lines = Common.getSampleDataAsArray 2022 20
+    // let lines = Common.getChallengeDataAsArray 2022 20
+    printAllLines lines
+    ()
