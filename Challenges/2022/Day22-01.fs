bab989b665086c1363c3c6d96c104499b9ed7c25
Howard Pinsley <howard.pinsley@blackstone.com>
2023-07-10 07:23:55 -0400

    break into two sections

--- Challenges/2022/Day22-01.fs
+++ Challenges/2022/Day22-01.fs
@@ -7,8 +7,15 @@ open System.Text.RegularExpressions
 open Microsoft.FSharp.Core.Operators.Checked
 open System.Collections.Generic
 
+let parseIntoModel (lines:string[]) : unit =
+    let l = lines.Length
+    let top = lines[0..(l - 3)]
+    let bottom = lines[l - 1]
+    ()
+
 let solve =
     let lines = Common.getSampleDataAsArray 2022 22
     // let lines = Common.getChallengeDataAsArray 2022 22
     printAllLines lines
+    parseIntoModel lines
     ()
