085ab23533a3d00fb5889f5ef098e9371b573ea0
Howard Pinsley <howard.pinsley@blackstone.com>
2023-07-14 08:19:29 -0400

    Create a state

--- Challenges/2022/Day23-01.fs
+++ Challenges/2022/Day23-01.fs
@@ -7,6 +7,23 @@ open System.Text.RegularExpressions
 open Microsoft.FSharp.Core.Operators.Checked
 open System.Collections.Generic
 
+let ROUNDS_TO_PLAY = 10
+
+type ElfInfo = {
+    id: int
+    location: (int * int)
+    proposedLocation: (int * int)
+}
+
+type State = 
+    {
+        elfCount: int
+        currentRound: int
+        roundsToPlay: int
+        elves: ElfInfo[]
+        occupiedLocations: Dictionary<(int * int), ElfInfo>
+    }
+
 let parseLines (lines:string[]) : (int * int) list =
     let parseLine (row:int) (line:string) : (int * int) seq =
        let occupied = line
@@ -24,6 +41,19 @@ let parseLines (lines:string[]) : (int * int) list =
                     |> Seq.toList
     allRows
 
+let buildState (occupied:(int * int) list) : State =
+    let elves = occupied
+                    |> Seq.mapi (fun id (row, col) -> { id = id; location = (row, col); proposedLocation = (-1, -1) })
+                    |> Seq.toArray
+    
+    {
+        elfCount = elves.Length
+        currentRound = 0
+        roundsToPlay = ROUNDS_TO_PLAY
+        elves = elves
+        occupiedLocations = Dictionary<(int * int), ElfInfo>(elves |> Seq.map (fun elf -> KeyValuePair(elf.location, elf)))
+    }
+
 let solve =
     let lines = Common.getSampleDataAsArray 2022 23
     // let lines = Common.getChallengeDataAsArray 2022 23
@@ -32,4 +62,6 @@ let solve =
 
     let occupied = parseLines lines
     printfn "%A" occupied
+    let state = buildState occupied
+    printfn "\nState:\n %A" state
     ()
