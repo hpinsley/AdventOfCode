8e16274619139c37534902289aacea049da7facc
Howard Pinsley <howard.pinsley@blackstone.com>
2023-07-19 07:19:29 -0400

    MobileBlizzard

--- Challenges/2022/Day24-01.fs
+++ Challenges/2022/Day24-01.fs
@@ -14,6 +14,12 @@ type Blizzard =
         motionDelta: (int * int)
     }
 
+type MoveableBlizzard =
+    {
+        initialPosition: Blizzard
+        getFutureLocation: int -> (int * int)
+    }
+
 type Cell =
     | Wall
     | Empty
@@ -24,6 +30,9 @@ type State =
         grid: Cell[,]
         rows: int
         cols: int
+        horizontalSnowCycleLength: int
+        verticalSnowCycleLength: int
+        blizzards: Blizzard[]
         start: int * int
         finish: int * int
     }
@@ -63,13 +72,91 @@ let findEmptyColumnInRow (grid:Cell[,]) (row:int) : int * int =
     let col = fst empty
     (row, col)
 
+(* The initial postion of a blizzard is considered
+    time zero.  The future position of the blizzard
+    at time t must account for the cylcle lengths and
+    the fact that the walls don't count.
+
+    So for a storm moving West -- with an col delta of -1
+    we must:
+    - First subtract 1 from the storms current column
+        to make it 0 based.
+    - Then subtract t from that location to see how
+        far it moved
+    - Take's it's modulo cycle length
+    - Add back the 1 to make it 1-based again
+*)
+let createMoveableBlizzard
+            (colFunc: int -> (int * int) -> int -> (int * int))
+            (rowFunc: int -> (int * int) -> int -> (int * int))
+            (blizzard:Blizzard) : MoveableBlizzard =
+            
+    let locator (t:int) =
+        match blizzard.motionDelta with
+            | (0, -1) | (0, 1) ->
+                let dc = snd blizzard.motionDelta
+                colFunc dc blizzard.position t
+
+            | (-1, 0) | (1, 0) ->
+                let dr = fst blizzard.motionDelta
+                rowFunc dr blizzard.position t
+            | _ ->
+                failwith "Unsupported movement delta"
+    
+    let moveableStorm =
+        {
+            initialPosition = blizzard
+            getFutureLocation = locator
+        }
+
+    moveableStorm
+
 let parseGridIntoState (grid:Cell[,]) : State =
     let rowCount = Array2D.length1 grid
     let colCount = Array2D.length2 grid
+
+    // Collect all blizzards and rather than deal with them
+    // in the grid, we should be able to determine their
+    // positions at any time t given the cycle length
+
+    // Don't include the border walls since the blizzards
+    // wrap around them
+
+    let horizontalSnowCycleLength = colCount - 2
+    let verticalSnowCycleLength = rowCount - 2
+    let mutable blizzards = []
+
+    grid |> Array2D.iter (fun cell ->
+                            match cell with
+                                | Snow bizzard ->
+                                    blizzards <- bizzard :: blizzards
+                                | _ -> ()
+                          ) 
+   
+    let vBlizzards = blizzards |> List.rev |> Array.ofList
+
+    let colFunc (dc:int) (loc:int * int) (t:int) : (int * int)=
+        let (row, col) = loc
+        (row, ((col - 1) + dc) % horizontalSnowCycleLength + 1)
+
+    let rowFunc (dr:int) (loc:int * int) (t:int) : (int * int) =
+        let (row, col) = loc
+        (((row - 1) + dr) % verticalSnowCycleLength + 1, col)
+
+
+        
+        
+    let factory = createMoveableBlizzard colFunc rowFunc
+
+    let moveableBlizzards = 
+        vBlizzards |> Array.map factory
     {
         grid = grid
         rows = rowCount
         cols = colCount
+        horizontalSnowCycleLength = horizontalSnowCycleLength
+        verticalSnowCycleLength = verticalSnowCycleLength
+        blizzards = vBlizzards
         start = findEmptyColumnInRow grid 0
         finish = findEmptyColumnInRow grid (rowCount - 1)
     }
@@ -119,9 +206,9 @@ let solve =
     let grid = parseLinesIntoGrid lines
     showTheGrid grid
     let state = parseGridIntoState grid
-    //printfn "State: %A" state
+    printfn "State: %A" state
     printfn "Start at: %A and finish at %A" state.start state.finish
 
-    let path = solveStateSimple state
-    printfn "Path:\n%A" path
+    //let path = solveStateSimple state
+    //printfn "Path:\n%A" path
     ()
