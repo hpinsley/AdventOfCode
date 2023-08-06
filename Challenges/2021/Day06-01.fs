ff7800ec6e41b1185c83aa1470f28060a3152833
Howard Pinsley <howard.pinsley@blackstone.com>
2023-08-06 12:36:45 -0400

    Finished day 6

--- Challenges/2021/Day06-01.fs
+++ Challenges/2021/Day06-01.fs
@@ -7,17 +7,17 @@ open System.Text.RegularExpressions
 open Microsoft.FSharp.Core.Operators.Checked
 open System.Collections.Generic
 
-let parseInput (lines:string[]) : int[] =
+let parseInput (lines:string[]) : int64[] =
     lines[0].Split(',') 
         |> Array.map int
         |> Array.fold (fun acc f -> 
-                        acc[f] <- acc[f] + 1
+                        acc[f] <- acc[f] + 1L
                         acc
                       )
-                        (Array.create 9 0)
+                        (Array.create 9 0L)
 
-let elapseDay (fish:int[]) : int[] =
-    let newFish = Array.create 9 0
+let elapseDay (fish:int64[]) : int64[] =
+    let newFish = Array.create 9 0L
     let spawningFish = fish[0]
     
     // Reduce day
@@ -37,7 +37,7 @@ let solve =
     let fish =  parseInput lines
     printfn "%A" fish
 
-    let finalFish = seq { 1 .. 80 } |> Seq.fold (fun fish _ -> elapseDay fish) fish
+    let finalFish = seq { 1 .. 256 } |> Seq.fold (fun fish _ -> elapseDay fish) fish
     printfn "%A" finalFish
 
     let result = finalFish |> Array.sum
