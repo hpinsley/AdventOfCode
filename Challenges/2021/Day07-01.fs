066711a9896cb08d505717a623bfb6b90a34ad39
Howard Pinsley <howard.pinsley@blackstone.com>
2023-08-06 17:29:11 -0400

    Solved day 7 part 1

--- Challenges/2021/Day07-01.fs
+++ Challenges/2021/Day07-01.fs
@@ -8,7 +8,23 @@ open Microsoft.FSharp.Core.Operators.Checked
 open System.Collections.Generic
 
 let solve =
-    let lines = Common.getSampleDataAsArray 2021 7
-    // let lines = Common.getChallengeDataAsArray 2021 7
+    // let lines = Common.getSampleDataAsArray 2021 7
+    let lines = Common.getChallengeDataAsArray 2021 7
+
     printAllLines lines
-    ()
+    let positions = lines[0].Split(',') |> Array.map int
+
+    printfn "%A" positions
+    let avg = (positions |> Array.sum |> double) / ((double) positions.Length)
+    printfn "There are %d positions with an average horizontal position = %f" positions.Length avg
+
+    let v1 = positions |> Array.min
+    let v2 = positions |> Array.max
+    printfn "Min %d; Max %d; Range: %d" v1 v2 (v2 - v1)
+
+    let moveResults = seq { v1 .. v2 }
+                        |> Seq.map (fun target ->
+                                        let moves = positions |> Array.map (fun p -> abs (p - target)) |> Array.sum
+                                        (target, moves)
+                                    )
+    printfn "%A" (moveResults |> Seq.minBy snd)
