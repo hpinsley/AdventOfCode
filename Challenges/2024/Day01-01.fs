ac10fc798441449a44509a62e9df9433b0e19533
Howard Pinsley <howard.pinsley@blackstone.com>
2025-02-24 08:08:38 -0500

    Split into two lists of numbers.

--- Challenges/2024/Day01-01.fs
+++ Challenges/2024/Day01-01.fs
@@ -7,36 +7,22 @@ open System.Text.RegularExpressions
 open Microsoft.FSharp.Core.Operators.Checked
 open System.Collections.Generic
 
-let countIncreased (values:int[]) : int =
-    let state = (0, Int32.MaxValue)
-    let (increaseCount, _) =
-        values |> Array.fold
-                    (fun (incCount, prevValue) v ->
-                        if (v > prevValue)
-                        then
-                            (incCount + 1, v)
-                        else
-                            (incCount, v)
-                    )
-                    state
-    increaseCount
+let getLists (lines:string[]) : int[] * int[] =
+    lines 
+        |> Array.map (fun s -> s.Split("   "))
+        |> Array.map (fun v -> Array.map (fun v2 -> parseInt(v2)) v)
+        |> Array.map (fun pair -> (pair[0], pair[1]))
+        |> Array.unzip
+    
 
 let part1 (lines:string[]) : unit =
-    let values = lines |> Array.map int
-    let result = countIncreased values
-    printfn "Part1: %d increases" result
-
-let part2 (lines:string[]) : unit =
-    let values = lines |> Array.map int
-    let windowed = values |> Array.windowed 3
-    let windowSums = windowed |> Array.map Array.sum
-    let result = countIncreased windowSums
-    printfn "Part2: %d increases" result
+    let (list1, list2) = getLists lines
+    ()
 
 let solve =
     //let lines = Common.getSampleDataAsArray 2021 1
-    let lines = Common.getChallengeDataAsArray 2021 1
+    let lines = Common.getSampleDataAsArray 2024 1
     part1(lines)
 
-    part2(lines)
+    //part2(lines)
     ()
