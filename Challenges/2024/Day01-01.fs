bc69999c1bf58cbf1ffc007632bccca05f57984d
Howard Pinsley <howard.pinsley@blackstone.com>
2025-02-24 08:20:33 -0500

    Solved 2024 day 1 part 1

--- Challenges/2024/Day01-01.fs
+++ Challenges/2024/Day01-01.fs
@@ -17,11 +17,18 @@ let getLists (lines:string[]) : int[] * int[] =
 
 let part1 (lines:string[]) : unit =
     let (list1, list2) = getLists lines
+    let sorted1 = list1 |> Seq.sort |> Seq.toArray
+    let sorted2 = list2 |> Seq.sort |> Seq.toArray
+
+    let diffs = Array.map2 (fun v1 v2 -> abs (v2 - v1)) sorted1 sorted2
+    let answer = diffs |> Seq.sum
+    dump "Answer" answer
     ()
 
 let solve =
     //let lines = Common.getSampleDataAsArray 2021 1
-    let lines = Common.getSampleDataAsArray 2024 1
+    //let lines = Common.getSampleDataAsArray 2024 1
+    let lines = Common.getChallengeDataAsArray 2024 1
     part1(lines)
 
     //part2(lines)
