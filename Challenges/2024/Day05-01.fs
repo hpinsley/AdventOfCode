3adb610329df9b4116b9fef6e8da8cdb8a3060e5
Howard Pinsley <howard.pinsley@blackstone.com>
2025-03-01 19:19:44 -0500

    Start day 5

---
+++ Challenges/2024/Day05-01.fs
@@ -0,0 +1,38 @@
+﻿module Year2024Day5_Part1
+
+open System
+open System.IO
+open Common
+open System.Text.RegularExpressions
+open Microsoft.FSharp.Core.Operators.Checked
+open System.Collections.Generic
+
+type OrderingRule =
+    {
+        predecessor: int;
+        successor: int;
+    }
+
+type UpdateList = int[]
+
+let solve =
+    let lines = Common.getSampleDataAsArray 2024 5
+    // let lines = Common.getChallengeDataAsArray 2024 5
+
+    let blankLineIndex = Array.findIndex (fun line -> line = "") lines
+    let topSection = lines[0..blankLineIndex - 1]
+    let bottomSection = lines[blankLineIndex+1..]
+
+    let orderingRules = topSection |> Array.map (fun line -> 
+                                                    let parts = line.Split [|'|'|]
+                                                    {
+                                                        predecessor = parseInt parts[0];
+                                                        successor = parseInt parts[1];
+                                                    })
+    // printfn "%A" orderingRules
+
+    let updateLists:UpdateList[] = bottomSection 
+                                    |> Array.map (fun line -> line.Split [|','|] 
+                                                                |> Array.map (fun chunk -> parseInt chunk)
+                                                 )
+    ()
--- Driver.fs
+++ Driver.fs
@@ -10,7 +10,8 @@ let main argv =
     //let x = Year2024Day1_Part1.solve
     //let x = Year2024Day2_Part1.solve
     //let x = Year2024Day3_Part1.solve
-    let x = Year2024Day4_Part1.solve
+    //let x = Year2024Day4_Part1.solve
+    let x = Year2024Day5_Part1.solve
 
     //let x = Year2021Day8_Part1.solve
     //let x = Year2021Day7_Part1.solve
