485981604b6168f2b5fd158b2c211278182a31de
Howard Pinsley <howard.pinsley@blackstone.com>
2025-03-04 08:26:25 -0500

    Done with Day 7 part 1

--- Challenges/2024/Day07-01.fs
+++ Challenges/2024/Day07-01.fs
@@ -8,25 +8,56 @@ open Microsoft.FSharp.Core.Operators.Checked
 open System.Collections.Generic
 open System.Diagnostics
 
+type Number = uint64
+
 type ProblemSet =
     {
-        target: uint64
-        arguments: uint64[]
+        target: Number
+        arguments: Number list
     }
 
-let part1 (problems:ProblemSet[]) : uint64 =
-    problems |> Array.sumBy (fun p -> p.target)
+type Operation =
+    | Add
+    | Multiply
+
+
+let operations = [| Add; Multiply |]
+
+let isSovable (problem:ProblemSet): bool =
+    
+    let target = problem.target
+    let currentResult = problem.arguments[0]
+    let remaining = List.tail problem.arguments
+
+    let rec trySolve (target:Number) (currentResult:Number) (remaining:Number list) : bool =
+        match remaining with
+            | [] -> currentResult = target
+            | head :: tail -> 
+                let addResult = currentResult + head
+                if trySolve target addResult tail
+                then
+                    true
+                else
+                    let mulResult = currentResult * head
+                    trySolve target mulResult tail
+
+    trySolve target currentResult remaining
+
+
+let part1 (problems:ProblemSet[]) : Number =
+    let solvable = problems |> Array.filter isSovable
+    solvable |> Array.sumBy (fun p -> p.target)
 
 let solve =
     let stopWatch = Stopwatch.StartNew()
-    let lines = Common.getSampleDataAsArray 2024 7
-    // let lines = Common.getChallengeDataAsArray 2024 7
+    //let lines = Common.getSampleDataAsArray 2024 7
+    let lines = Common.getChallengeDataAsArray 2024 7
     
     let problems = lines 
                     |> Array.map (fun line -> line.Replace(":", ""))
                     |> Array.map (fun line -> line.Split(' '))
                     |> Array.map (fun strings -> Array.map UInt64.Parse strings)
-                    |> Array.map (fun vNums -> { target = vNums[0]; arguments = vNums[1..]})
+                    |> Array.map (fun vNums -> { target = vNums[0]; arguments = Array.toList vNums[1..]})
     
     let part1Result = part1 problems
 
