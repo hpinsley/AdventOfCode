ced5759cc2a5a624010c833033fa77ed094b8dce
Howard Pinsley <howard.pinsley@blackstone.com>
2025-03-06 08:13:53 -0500

    Part 1 WIP

--- Challenges/2024/Day09-01.fs
+++ Challenges/2024/Day09-01.fs
@@ -8,13 +8,70 @@ open Microsoft.FSharp.Core.Operators.Checked
 open System.Collections.Generic
 open System.Diagnostics
 
+let FREE_BLOCK = -1
+
+type CompactEntry =
+    | Empty of int
+    | File of (int * int)
+
+type State =
+    {
+        blockContents: int[]
+        freeList: List<int>
+        totalBlocks: int
+    }
+
+let buildState (entries:CompactEntry[]) : State =
+    let totalBlocks = entries |> Array.sumBy (fun entry -> match entry 
+                                                            with 
+                                                                | Empty size -> size
+                                                                | File (fileno, size) -> size
+                                             )
+    
+    let blockContents = Array.create totalBlocks FREE_BLOCK
+    let freeList = List<int>()
+
+    let mutable nextBlock = 0
+    for entry in entries do
+        match entry with
+            | File (fileno, size) ->
+                Array.fill blockContents nextBlock size fileno
+                nextBlock <- nextBlock + size
+            | Empty size ->
+                for b in { nextBlock .. nextBlock + size - 1} do
+                    freeList.Add(b)
+                nextBlock <- nextBlock + size
+
+    printfn "The disk contains %d blocks" totalBlocks
+    {
+        blockContents = blockContents;
+        freeList = freeList
+        totalBlocks = totalBlocks
+    }
+
+
+let part1 (line:string) : uint64 =
+    
+    let isFile compactIndex = compactIndex % 2 = 0
+
+    let compactMap = line |> Array.ofSeq |> Array.map (fun c -> parseInt (c.ToString()))
+    let parsedMap = Array.mapi (fun index v ->
+                                    if isFile index
+                                    then
+                                        File (index / 2, v)
+                                    else
+                                        Empty v) 
+                                compactMap
+    let initialState = buildState parsedMap
+    0UL
+
 let solve =
     let stopWatch = Stopwatch.StartNew()
 
-    // let lines = Common.getSampleDataAsArray 2024 9
-    let lines = Common.getChallengeDataAsArray 2024 9
+    let lines = Common.getSampleDataAsArray 2024 9
+    // let lines = Common.getChallengeDataAsArray 2024 9
 
-    let compactMap = lines[0] |> Array.ofSeq |> Array.map (fun c -> parseInt (c.ToString()))
+    let part1Result = part1 lines[0]
 
     let part1Time = stopWatch.ElapsedMilliseconds
  
