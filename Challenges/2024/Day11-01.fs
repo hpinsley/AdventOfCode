a66a0102ada3dd73c6647731c3e8b70c4d949552
Howard Pinsley <hpinsley@gmail.com>
2025-03-09 19:18:32 +0000

    Day 11 Part 1 done

--- Challenges/2024/Day11-01.fs
+++ Challenges/2024/Day11-01.fs
@@ -8,13 +8,49 @@ open Microsoft.FSharp.Core.Operators.Checked
 open System.Collections.Generic
 open System.Diagnostics
 
+type T_NUMBER = uint64
+type T_STONE = T_NUMBER
+
+let splitEvenStone (stone:T_STONE) : (T_STONE * T_STONE) option =
+    let label = stone.ToString()
+    if label.Length % 2 = 0 then
+        let left = label[0..label.Length / 2 - 1] |> UInt64.Parse
+        let right = label[label.Length / 2..] |> UInt64.Parse
+        Some (left, right)
+    else
+        None
+let changeStone (stone:T_STONE) : T_STONE list =
+    if stone = 0UL then
+        [1UL]
+    else
+        match splitEvenStone stone with
+        | Some (left, right) -> [left; right]
+        | None -> [stone * 2024UL]
+
+let blink (stones:T_STONE list) : T_STONE list =
+    stones |> List.collect changeStone
+
+let rec part1 (stones:T_STONE list) (blinks:int): int =
+    if blinks = 0
+    then
+        stones.Length
+    else
+        let expandedStones = blink stones
+        part1 expandedStones (blinks - 1)
+
+
 let solve =
     let stopWatch = Stopwatch.StartNew()
 
-    let line = Common.getSampleData 2024 11
-    // let line = Common.getChallengeData 2024 11
+    // let line = Common.getSampleData 2024 11
+    let line = Common.getChallengeData 2024 11
+    let stones = line.Split(" ") |> Array.map uint64 |> List.ofArray
 
     printfn "Line: %s" line
+
+    let blinkTimes = 25
+    let part1Result = part1 stones blinkTimes
+    printfn "Part 1: %d" part1Result
 
     let part1Time = stopWatch.ElapsedMilliseconds
 
