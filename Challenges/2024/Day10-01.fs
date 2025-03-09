dd1a4891522d55aac41f3f64161a9b53335cf918
Howard Pinsley <hpinsley@gmail.com>
2025-03-09 15:56:17 +0000

    day 10 wip

--- Challenges/2024/Day10-01.fs
+++ Challenges/2024/Day10-01.fs
@@ -8,6 +8,21 @@ open Microsoft.FSharp.Core.Operators.Checked
 open System.Collections.Generic
 open System.Diagnostics
 
+type Location = 
+    { 
+        row: int
+        col: int
+    }
+
+let getNeighbors (rows:int) (cols:int) (location:Location) : Location list =
+    let possibles = [ 
+                      { row = location.row - 1; col = location.col  }
+                      { row = location.row + 1; col = location.col }
+                      { row = location.row; col = location.col - 1 }
+                      { row = location.row; col = location.col + 1 }
+    ]
+    possibles |> List.filter (fun loc -> loc.row >= 0 && loc.row < rows && loc.col >= 0 && loc.col < cols)
+
 let solve =
     let stopWatch = Stopwatch.StartNew()
 
@@ -18,8 +33,9 @@ let solve =
     let cols = lines[0].Length
     printf "Rows: %d, Cols: %d\n" rows cols
 
-    let grid = Array2D.init rows cols (fun i j -> lines[i][j])
-    printGrid grid id
+    let grid = Array2D.init rows cols (fun i  j ->  parseInt ((lines[i][j]).ToString()))
+    printGrid grid (fun i -> i.ToString()[0])   // We only expect single digit numbers
+
     let part1Time = stopWatch.ElapsedMilliseconds
 
     stopWatch.Restart()
