a9845c61c715f3fab95dc376569d07f166cf22b1
Howard Pinsley <hpinsley@gmail.com>
2025-03-09 18:53:59 +0000

    Start day 11

---
+++ Challenges/2024/Day11-01.fs
@@ -0,0 +1,28 @@
+﻿module Year2024Day11_Part1
+
+open System
+open System.IO
+open Common
+open System.Text.RegularExpressions
+open Microsoft.FSharp.Core.Operators.Checked
+open System.Collections.Generic
+open System.Diagnostics
+
+let solve =
+    let stopWatch = Stopwatch.StartNew()
+
+    let line = Common.getSampleData 2024 11
+    // let line = Common.getChallengeData 2024 11
+
+    printfn "Line: %s" line
+
+    let part1Time = stopWatch.ElapsedMilliseconds
+
+    stopWatch.Restart()
+
+    // Part 2
+    let part2Time = stopWatch.ElapsedMilliseconds;
+
+    printfn "Timings.  Part 1: %dms, Part 2: %dms" part1Time part2Time
+
+    ()
--- Driver.fs
+++ Driver.fs
@@ -16,7 +16,8 @@ let main argv =
     //let x = Year2024Day7_Part1.solve
     //let x = Year2024Day8_Part1.solve
     // let x = Year2024Day9_Part1.solve
-    let x = Year2024Day10_Part1.solve
+    // let x = Year2024Day10_Part1.solve
+    let x = Year2024Day11_Part1.solve
 
     //let x = Year2021Day8_Part1.solve
     //let x = Year2021Day7_Part1.solve
