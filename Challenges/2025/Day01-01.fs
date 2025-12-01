665fbc62d94b6fe2c47200ab830b5449f4179bd9
Howard Pinsley <howard.pinsley@blackstone.com>
2025-12-01 18:28:52 -0500

    Starting day 1 2025

--- Challenges/2025/Day01-01.fs
+++ Challenges/2025/Day01-01.fs
@@ -7,6 +7,22 @@ open System.Text.RegularExpressions
 open Microsoft.FSharp.Core.Operators.Checked
 open System.Collections.Generic
 
+type Rotation = (int * int) // (direction, steps)
+
+let parseRotation (instruction:string) : Rotation =
+    let m = Regex("(R|L)(\d+)").Match(instruction)
+    if (not m.Success)
+    then
+        failwith "Bad match"
+
+    let direction = if m.Groups[1].Value = "R" then 1 else -1
+    let steps = int m.Groups[2].Value
+    (direction, steps)
+
 let solve =
-    printfn "Part 1: Result is %A" "not ready"
+    let lines = Common.getSampleDataAsArray 2025 1
+    // let text = Common.getChallengeDataAsArray 2025 1
+    printfn "Input text: %A" lines
+    let rotations = lines |> Array.map parseRotation
+    printfn "Parsed rotations: %A" rotations
     ()
