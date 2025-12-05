dbdda43c0029a30bd990ace9f002a9e442e6fe06
Howard Pinsley <howard.pinsley@blackstone.com>
2025-12-05 07:47:32 -0500

    Add tests to figure out what's wrong with day1 part 2

--- Challenges/2025/Day01-01.fs
+++ Challenges/2025/Day01-01.fs
@@ -8,29 +8,60 @@ open Microsoft.FSharp.Core.Operators.Checked
 open System.Collections.Generic
 
 let DIAL_SIZE = 100
+let STARTING_NUMBER = 50
 
 type State =
     {
         currentNumber: int
         moveCount: int
         zeroCount: int
+        zeroClicks: int
     }
 
 type Rotation = (int * int)
 
 let processPart1Rotation (state: State) (rotation: Rotation) : State =
+
     let (direction, magnitude) = rotation
-    // let currentNumber = (((state.currentNumber + direction * magnitude) % DIAL_SIZE) + DIAL_SIZE) % DIAL_SIZE
- 
-    let rotatedTo = state.currentNumber + direction * magnitude
-    let turns = (direction * magnitude |> abs) / DIAL_SIZE
     let currentNumber = (((state.currentNumber + direction * magnitude) % DIAL_SIZE) + DIAL_SIZE) % DIAL_SIZE
 
     { state with 
         currentNumber = currentNumber;
         moveCount = state.moveCount + 1;
-        zeroCount = if state.currentNumber = 0 then state.zeroCount + 1 else state.zeroCount
+        zeroCount = if currentNumber = 0 then state.zeroCount + 1 else state.zeroCount
     }
+
+let processPart2Rotation (state: State) (rotation: Rotation) : State =
+    let (direction, magnitude) = rotation
+ 
+    let turns = magnitude / DIAL_SIZE
+    let currentNumber = (((state.currentNumber + direction * magnitude) % DIAL_SIZE) + DIAL_SIZE) % DIAL_SIZE
+
+    let toAdd = if currentNumber = 0
+                        then
+                            if state.currentNumber = 0
+                            then
+                                0
+                            else
+                                1
+                        else
+                            if direction < 0
+                            then
+                                if state.currentNumber <> 0 && currentNumber > state.currentNumber then 1 else 0
+                            else
+                                if state.currentNumber <> 0 && currentNumber < state.currentNumber then 1 else 0
+    
+    let zeroClicks = turns + toAdd         
+
+    let newState = { state with 
+                                    currentNumber = currentNumber;
+                                    moveCount = state.moveCount + 1;
+                                    zeroCount = if currentNumber = 0 then state.zeroCount + 1 else state.zeroCount
+                                    zeroClicks = state.zeroClicks + zeroClicks
+                                }
+
+    printfn "%A %A %A\n=============" state rotation newState
+    newState
 
 let parseRotation (instruction:string) : Rotation =
     let m = Regex("(R|L)(\d+)").Match(instruction)
@@ -43,7 +74,11 @@ let parseRotation (instruction:string) : Rotation =
     (direction, steps)
 
 let part1 (rotations:Rotation[]) : State =
-    let finalState = rotations |> Array.fold processPart1Rotation { currentNumber = 50; moveCount = 0; zeroCount = 0 }
+    let finalState = rotations |> Array.fold processPart1Rotation { currentNumber = STARTING_NUMBER; moveCount = 0; zeroCount = 0; zeroClicks = 0 }
+    finalState
+
+let part2 (rotations:Rotation[]) : State =
+    let finalState = rotations |> Array.fold processPart2Rotation { currentNumber = STARTING_NUMBER; moveCount = 0; zeroCount = 0; zeroClicks = 0 }
     finalState
 
 let solve =
@@ -52,7 +87,10 @@ let solve =
     printfn "Input text: %A" lines
     let rotations = lines |> Array.map parseRotation
 
-    let part1Result = part1 rotations
-    // let finalState = rotations |> Array.fold 
-    printfn "Part1 result: %A" part1Result
+    // let part1Result = part1 rotations
+    // printfn "Part1 result: %A" part1Result
+
+    let part2Result = part2 rotations
+    // printfn "Part2 result: %A" part2Result
+
     ()
---
+++ Tests/Year2025Day1_Part2.fs
@@ -0,0 +1,79 @@
+﻿module Tests.Year2025Day1_Part2
+
+open System
+open Xunit
+open Year2025Day1_Part1
+
+
+[<Fact>]
+let ``can turn left and land on zero`` () =
+    let state:State =
+        {
+            currentNumber = 55; moveCount = 0; zeroCount = 0; zeroClicks = 0
+        }
+
+    let rotation = (-1, 55)
+    let nextState = processPart2Rotation state rotation
+
+    Assert.Equal(1, nextState.zeroClicks)
+
+[<Fact>]
+let ``can turn left one full rotation`` () =
+    let state:State =
+        {
+            currentNumber = 50; moveCount = 0; zeroCount = 0; zeroClicks = 0
+        }
+
+    let rotation = (-1, DIAL_SIZE)
+    let nextState = processPart2Rotation state rotation
+
+    Assert.Equal(1, nextState.zeroClicks)
+
+[<Fact>]
+let ``can turn left one full rotation at zero`` () =
+    let state:State =
+        {
+            currentNumber = 0; moveCount = 0; zeroCount = 0; zeroClicks = 0
+        }
+
+    let rotation = (-1, DIAL_SIZE)
+    let nextState = processPart2Rotation state rotation
+
+    Assert.Equal(1, nextState.zeroClicks)
+
+[<Fact>]
+let ``does not double count when going left from 0`` () =
+    let state:State =
+        {
+            currentNumber = 0; moveCount = 0; zeroCount = 0; zeroClicks = 0
+        }
+
+    let rotation = (-1, 1)
+    let nextState = processPart2Rotation state rotation
+
+    Assert.Equal(99, nextState.currentNumber)
+    Assert.Equal(0, nextState.zeroClicks)
+
+[<Fact>]
+let ``can turn right one full rotation at zero`` () =
+    let state:State =
+        {
+            currentNumber = 0; moveCount = 0; zeroCount = 0; zeroClicks = 0
+        }
+
+    let rotation = (1, DIAL_SIZE)
+    let nextState = processPart2Rotation state rotation
+
+    Assert.Equal(1, nextState.zeroClicks)
+
+[<Fact>]
+let ``can do 50 r 1000`` () =
+    let state:State =
+        {
+            currentNumber = 50; moveCount = 0; zeroCount = 0; zeroClicks = 0
+        }
+
+    let rotation = (1, 1000)
+    let nextState = processPart2Rotation state rotation
+
+    Assert.Equal(10, nextState.zeroClicks)
