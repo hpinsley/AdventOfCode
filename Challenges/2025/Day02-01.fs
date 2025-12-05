f48bad7540f72367f37e36e8e77ea57826aec214
Howard Pinsley <howard.pinsley@blackstone.com>
2025-12-05 17:53:41 -0500

    Start 2025 Day 2

---
+++ Challenges/2025/Day02-01.fs
@@ -0,0 +1,51 @@
+module Year2025Day2_Part1
+
+open System
+open System.IO
+open Common
+open System.Text.RegularExpressions
+open Microsoft.FSharp.Core.Operators.Checked
+open System.Collections.Generic
+
+type Number = uint64
+
+type Range = 
+    {
+        low: Number
+        high: Number 
+    }
+
+
+let part1 () : int =
+    1
+
+let part2 () : int =
+    2
+
+let parseForRanges (line:string) : Range[] =
+    line.Split(',')
+        |> Array.map (fun s -> 
+                        let parts = s.Split("-")
+                        { low = UInt64.Parse parts[0]; high = UInt64.Parse parts[1] })
+
+
+let solve =
+    // let line = Common.getSampleData 2025 2
+    let line = Common.getChallengeData 2025 2
+    // printfn "Input text: %A" line
+
+    let ranges = parseForRanges line
+
+    for range in ranges do
+        printfn "%u - %u: %u values" range.low range.high (range.high - range.low)
+
+    let numberCount = ranges |> Array.sumBy (fun r -> r.high - r.low)
+    printfn "Total numbers: %u" numberCount
+
+    // let part1Result = part1()
+    // printfn "Part1 result: %A" part1Result
+
+    // let part2Result = part2()
+    // printfn "Part2 result: %A" part2Result
+
+    ()
--- Driver.fs
+++ Driver.fs
@@ -5,9 +5,10 @@ open System.Text.RegularExpressions
 
 
 [<EntryPoint>]
-let main argv =
+let main (argv: string array) =
     printfn "Main driver"
-    let x = Year2025Day1_Part1.solve
+    // let x = Year2025Day1_Part1.solve
+    let x = Year2025Day2_Part1.solve
     
     //let x = Year2024Day1_Part1.solve
     //let x = Year2024Day2_Part1.solve
