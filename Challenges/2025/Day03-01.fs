e625b9a045a9f7be049d9d28ee3b13e76325dd07
Howard Pinsley <howard.pinsley@blackstone.com>
2025-12-29 07:48:55 -0500

    Completed 2025 Day 3 Part 1

--- Challenges/2025/Day03-01.fs
+++ Challenges/2025/Day03-01.fs
@@ -7,10 +7,25 @@ open System.Text.RegularExpressions
 open Microsoft.FSharp.Core.Operators.Checked
 open System.Collections.Generic
 
+let getMaxVoltage (digits:string) : int =
+    let maxJoltage = seq {
+                        for i in 0 .. digits.Length - 1 do
+                            for j in (i+1) .. digits.Length - 1 -> digits[i].ToString() + digits[j].ToString()
+                        }   |> Seq.sortDescending
+                            |> Seq.head
+                            |> Int32.Parse
+
+    printfn "Max joltage: %d" maxJoltage
+    maxJoltage
+
 let solve =
     // let lines = Common.getSampleDataAsArray 2025 3
     let lines = Common.getChallengeDataAsArray 2025 3
     printfn "Input text: %A" lines
+
+    let joltages = lines |> Array.map getMaxVoltage
+    let maxJoltage = Array.sum joltages
+    printfn "Max joltage is %d" maxJoltage
 
     // let part1Result = part1 ranges
     // printfn "Part1 result: %A" part1Result
