b4ddc8150a4135847b64c275c1fa398f641ce22a
Howard Pinsley <howard.pinsley@blackstone.com>
2026-02-05 08:25:31 -0500

    Day 6 WIP

--- Challenges/2025/Day06-01.fs
+++ Challenges/2025/Day06-01.fs
@@ -31,24 +31,10 @@ let transposeStrings (components:string[][]) : string[,] =
     let cols = components[0].Length
     printfn "Rows: %d, Cols: %d" rows cols
 
-    let x = Array2D.init cols rows (fun r c -> components[c][r])
-    x
+    let twoDArray = Array2D.init cols rows (fun r c -> components[c][r])
+    twoDArray
     
-
-// let parseInputData (lines:string[]): Problem[] =
-//     let x = lines
-//                     |> Array.map 
-//     [||]
-
-
-let solve =
-    let stopWatch = Stopwatch.StartNew()
-
-    let lines = Common.getSampleDataAsArray 2025 6
-    // let lines: string array = Common.getChallengeDataAsArray 2025 6
-
-    printfn "%A" lines
-
+let parseInputData (lines:string[]): Problem[] =
     let composed = parseComponents lines
     printfn "Composed"
     printfn "%A" composed
@@ -58,5 +44,26 @@ let solve =
     let transposed = transposeStrings composed
     printfn "Transposed"
     printfn "%A" transposed
+    let rowCount = Array2D.length1 transposed
+    let colCount = Array2D.length2 transposed
+    printfn "There are %d rows and %d cols" rowCount colCount
+
+    let x = seq {0..rowCount-1} 
+                            |> Seq.map (fun r -> 
+                                            let op = transposed[r,colCount - 1]
+                                            op
+                                        )  
+
+    [||]
+
+
+let solve =
+    let stopWatch = Stopwatch.StartNew()
+
+    // let lines = Common.getSampleDataAsArray 2025 6
+    let lines: string array = Common.getChallengeDataAsArray 2025 6
+
+    printfn "%A" lines
+    let x = parseInputData lines
 
     ()
