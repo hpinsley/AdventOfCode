f2cdd38cfda9ab59cf4eb65e98bd9a8aadfb8e24
Howard Pinsley <howard.pinsley@blackstone.com>
2026-02-05 07:56:15 -0500

    Start Day 6

---
+++ Challenges/2025/Day06-01.fs
@@ -0,0 +1,48 @@
+﻿module Year2025Day6_Part1
+
+open System
+open System.IO
+open Common
+open System.Text.RegularExpressions
+open Microsoft.FSharp.Core.Operators.Checked
+open System.Collections.Generic
+open System.Diagnostics
+
+type Operation =
+    | Add
+    | Multiply
+
+type Operand = int64
+type Problem = Operation of Operand[]
+
+let transposeStrings (lines:string[]) : string[][] =
+    let x = lines 
+                |> Array.map (fun line -> 
+                                let trimmedLine = line.Trim()
+                                let parts = trimmedLine.Split(" ") 
+                                let components = Array.filter (fun s -> not (String.IsNullOrWhiteSpace(s))) parts
+
+                                components
+                             )
+    x
+
+// let parseInputData (lines:string[]): Problem[] =
+//     let x = lines
+//                     |> Array.map 
+//     [||]
+
+
+let solve =
+    let stopWatch = Stopwatch.StartNew()
+
+    let lines = Common.getSampleDataAsArray 2025 6
+    // let lines: string array = Common.getChallengeDataAsArray 2025 6
+
+    printfn "%A" lines
+
+    let x = transposeStrings lines
+    printfn "Transposed"
+
+    printfn "%A" x
+
+    ()
--- Driver.fs
+++ Driver.fs
@@ -10,7 +10,8 @@ let main (argv: string array) =
     // let x = Year2025Day1_Part1.solve
     // let x = Year2025Day2_Part1.solve
     // let x = Year2025Day4_Part1.solve
-    let x = Year2025Day5_Part1.solve
+    // let x = Year2025Day5_Part1.solve
+    let x = Year2025Day6_Part1.solve
     
     // let x = Year2025Day3_Part1.solve
     //let x = Year2024Day1_Part1.solve
