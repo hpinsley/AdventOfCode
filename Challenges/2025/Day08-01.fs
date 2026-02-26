3009b4bb9a1710ab1aa459eb03dcc5ddb9d0b075
Howard Pinsley <howard.pinsley@blackstone.com>
2026-02-25 19:28:11 -0500

    Day 8 wip

--- Challenges/2025/Day08-01.fs
+++ Challenges/2025/Day08-01.fs
@@ -8,11 +8,34 @@ open Microsoft.FSharp.Core.Operators.Checked
 open System.Collections.Generic
 open System.Diagnostics
 
+type DISTANCE = uint64
+
+type BOX_LOC = {
+    x: DISTANCE
+    y: DISTANCE
+    z: DISTANCE
+}
+
+let parseLine (line:string) : BOX_LOC =
+    let splitData = line.Split(",")
+    let result = { 
+        x = UInt64.Parse(splitData[0]); 
+        y = UInt64.Parse(splitData[1]); 
+        z = UInt64.Parse(splitData[2]); 
+    }
+    result
+
+let parseInputData (lines:string[]) : BOX_LOC[] =
+    lines |> Array.map parseLine
+
 let solve =
     let stopWatch = Stopwatch.StartNew()
 
     let lines = Common.getSampleDataAsArray 2025 8
     // let lines: string array = Common.getChallengeDataAsArray 2025 8
     printfn "%A" lines
+
+    let parsed = parseInputData lines
+    printfn "%A" parsed
 
     ()
