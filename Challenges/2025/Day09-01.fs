2b5ca2a9e166cfb6e67c1821a1e9e723193e512b
Howard Pinsley <howard.pinsley@blackstone.com>
2026-03-02 09:02:39 -0500

    Parse input

--- Challenges/2025/Day09-01.fs
+++ Challenges/2025/Day09-01.fs
@@ -8,6 +8,13 @@ open Microsoft.FSharp.Core.Operators.Checked
 open System.Collections.Generic
 open System.Diagnostics
 
+type LOC_TYPE = UInt64
+
+type TileLocation = {
+    x: LOC_TYPE
+    y: LOC_TYPE
+}
+
 let solve =
     let stopWatch = Stopwatch.StartNew()
 
@@ -15,4 +22,14 @@ let solve =
     // let lines: string array = Common.getChallengeDataAsArray 2025 9
 
     printfn "%A" lines
+    let locations = lines
+                        |> Array.map (fun (s:string) ->
+                                        let split = s.Split(",")
+                                        { 
+                                            x = UInt64.Parse(split[0])
+                                            y = UInt64.Parse(split[1])
+                                        }
+                                     )
+
+    printfn "%A" locations
     ()
